f2a7de9dfe135889853be5163fbc763f47d96983
Yasuhisa Tanioka <Yasuhisa.Tanioka@sony.com>
2019-06-05 22:15:36 +0900

    Change MarkerLineEntity position from GOCS to GeoPoint

--- src/mapray/MarkerLineEntity.js
+++ src/mapray/MarkerLineEntity.js
@@ -3,6 +3,7 @@ import Primitive from "./Primitive";
 import Mesh from "./Mesh";
 import MarkerLineMaterial from "./MarkerLineMaterial";
 import GeoMath from "./GeoMath";
+import GeoPoint from "./GeoPoint";
 
 
 /**
@@ -104,9 +105,11 @@ class MarkerLineEntity extends Entity {
 
     /**
      * @summary 複数の頂点を追加
+     *
      * @desc
-     * <p>points は [x0, y0, z0, x1, y1, z1, ...] のような形式の配列 (座標系は GOCS) を与える。</p>
-     * @param {array.<number>} points  頂点の配列
+     * <p>points は [lon_0, lat_0, alt_0, lon_1, lat_1, alt_1, ...] のような形式の配列を与える。</p>
+     *
+     * @param {number[]} points  頂点の配列
      */
     addPoints( points )
     {
@@ -178,11 +181,16 @@ class MarkerLineEntity extends Entity {
             return;
         }
 
+        // this._buffer を GOCS に変換した配列を生成
+        var  num_points = this._num_floats / 3;
+        var gocs_buffer = GeoPoint.toGocsArray( this._buffer, num_points,
+                                                new Float64Array( this._num_floats ) );
+
         // プリミティブの更新
         //   primitive.transform
         //   primitive.pivot
         //   primitive.bbox
-        this._updateTransformPivotBBox();
+        this._updateTransformPivotBBox( gocs_buffer, num_points );
 
         // メッシュ生成
         var mesh_data = {
@@ -191,7 +199,7 @@ class MarkerLineEntity extends Entity {
                 { name: "a_direction", size: 3 },
                 { name: "a_where",     size: 2 }
             ],
-            vertices: this._createVertices(),
+            vertices: this._createVertices( gocs_buffer, num_points ),
             indices:  this._createIndices()
         };
         var mesh = new Mesh( this.scene.glenv, mesh_data );
@@ -211,25 +219,25 @@ class MarkerLineEntity extends Entity {
 
     /**
      * @summary プリミティブの更新
+     *
      * @desc
-     * 条件: this._num_floats >= 3
-     * 入力:
-     *   this._buffer
-     *   this._num_floats
+     * <pre>
      * 出力:
      *   this._transform
      *   this._pivot
      *   this._bbox
+     * </pre>
+     *
+     * @param {Float64Array} gocs_buffer  入力頂点配列 (GOCS)
+     * @param {number}       num_points   入力頂点数
      * @private
      */
-    _updateTransformPivotBBox()
+    _updateTransformPivotBBox( gocs_buffer, num_points )
     {
-        var buffer = this._buffer;
-
         // モデル座標系の原点 (GOCS)
-        var ox = buffer[0];
-        var oy = buffer[1];
-        var oz = buffer[2];
+        var ox = gocs_buffer[0];
+        var oy = gocs_buffer[1];
+        var oz = gocs_buffer[2];
 
         // 変換行列の更新
         var transform = this._transform;
@@ -238,8 +246,6 @@ class MarkerLineEntity extends Entity {
         transform[14] = oz;
 
         // 統計
-        var num_points = this._num_floats / 3;
-
         var xsum = 0;
         var ysum = 0;
         var zsum = 0;
@@ -254,9 +260,9 @@ class MarkerLineEntity extends Entity {
 
         for ( var i = 0; i < num_points; ++i ) {
             var b = 3 * i;
-            var x = buffer[b]     - ox;
-            var y = buffer[b + 1] - oy;
-            var z = buffer[b + 2] - oz;
+            var x = gocs_buffer[b]     - ox;
+            var y = gocs_buffer[b + 1] - oy;
+            var z = gocs_buffer[b + 2] - oz;
 
             xsum += x;
             ysum += y;
@@ -291,40 +297,35 @@ class MarkerLineEntity extends Entity {
 
 
     /**
-     * @summary 頂点配列は生成
-     * @desc
-     * 条件: this._num_floats >= 6
-     * 入力:
-     *   this._buffer
-     *   this._num_floats
-     * @return {Float32Array}  頂点配列
+     * @summary 頂点配列の生成
+     *
+     * @param  {Float64Array} gocs_buffer  入力頂点配列 (GOCS)
+     * @param  {number}       num_points   入力頂点数
+     * @return {Float32Array}              Mesh 用の頂点配列
      * @private
      */
-    _createVertices()
+    _createVertices( gocs_buffer, num_points )
     {
-        var buffer = this._buffer;
-
         // モデル座標系の原点 (GOCS)
-        var ox = buffer[0];
-        var oy = buffer[1];
-        var oz = buffer[2];
+        var ox = gocs_buffer[0];
+        var oy = gocs_buffer[1];
+        var oz = gocs_buffer[2];
 
-        var num_points   = this._num_floats / 3;
         var num_segments = num_points - 1;
         var num_vertices = 4 * num_segments;
         var vertices     = new Float32Array( 8 * num_vertices );
 
         for ( var i = 0; i < num_segments; ++i ) {
             var  b = 3 * i;
-            var sx = buffer[b]     - ox;
-            var sy = buffer[b + 1] - oy;
-            var sz = buffer[b + 2] - oz;
-            var ex = buffer[b + 3] - ox;
-            var ey = buffer[b + 4] - oy;
-            var ez = buffer[b + 5] - oz;
-            var dx = buffer[b + 3] - buffer[b];
-            var dy = buffer[b + 4] - buffer[b + 1];
-            var dz = buffer[b + 5] - buffer[b + 2];
+            var sx = gocs_buffer[b]     - ox;
+            var sy = gocs_buffer[b + 1] - oy;
+            var sz = gocs_buffer[b + 2] - oz;
+            var ex = gocs_buffer[b + 3] - ox;
+            var ey = gocs_buffer[b + 4] - oy;
+            var ez = gocs_buffer[b + 5] - oz;
+            var dx = gocs_buffer[b + 3] - gocs_buffer[b];
+            var dy = gocs_buffer[b + 4] - gocs_buffer[b + 1];
+            var dz = gocs_buffer[b + 5] - gocs_buffer[b + 2];
             var  v = 32 * i;
 
             // 始左
@@ -406,7 +407,7 @@ class MarkerLineEntity extends Entity {
     _setupByJson( json )
     {
         // json.points
-        this.addPoints( MarkerLineEntity._toCartesianPoints( json.points ) );
+        this.addPoints( json.points );
 
         // json.line_width
         //     .color
@@ -415,54 +416,6 @@ class MarkerLineEntity extends Entity {
         if ( json.line_width ) props.width = json.line_width;
         if ( json.color )      GeoMath.copyVector3( json.color, props.color );
         if ( json.opacity )    props.opacity = json.opacity;
-    }
-
-
-    /**
-     * @summary 頂点配列を GOCS に変換
-     * @param  {object} points  入力頂点
-     * @return {array}          変換結果
-     * @private
-     */
-    static _toCartesianPoints( points )
-    {
-        var   type = points.type || "cartesian";
-        var coords = points.coords;
-        var    dst = null;
-
-        if ( type == "cartesian" ) {
-            // 変換なし
-            dst = coords;
-        }
-        else if ( type == "cartographic" ) {
-            // x == (R + h)Cos[φ]Cos[λ]
-            // y == (R + h)Cos[φ]Sin[λ]
-            // z == (R + h)Sin[φ]
-
-            var degree = GeoMath.DEGREE;
-            var radius = GeoMath.EARTH_RADIUS;
-            var length = coords.length;
-            dst = new Float64Array( length );
-
-            for ( var i = 0; i < length; i += 3 ) {
-                var λ = coords[i]     * degree;
-                var φ = coords[i + 1] * degree;
-                var  r = coords[i + 2] + radius;
-                var sinλ = Math.sin( λ );
-                var cosλ = Math.cos( λ );
-                var sinφ = Math.sin( φ );
-                var cosφ = Math.cos( φ );
-                dst[i]     = r * cosφ * cosλ;
-                dst[i + 1] = r * cosφ * sinλ;
-                dst[i + 2] = r * sinφ;
-            }
-        }
-        else {
-            console.error( "mapray: unknown points type: " + type );
-            dst = [];
-        }
-
-        return dst;
     }
 
 }
