87e1a89ca638e47f3528204a2a89896715b147f8
Daisuke Matsumoto <Daisuke.Matsumoto@jp.sony.com>
2020-03-12 09:08:21 +0900

    fixed deploy method in setup_deploy.sh and update_package_json

--- _deploy/scripts/setup_deploy.sh
+++ _deploy/scripts/setup_deploy.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
-HOME=$(cd $(dirname $0)/../..; pwd)
-DIST=$(cd $(dirname $0)/../../dist; pwd)
-DEPLOY_DIR=$(cd $(dirname $0)/../public; pwd)
-PUBLIC=${DEPLOY_DIR}/mapray-js
-RESOURCE_VERSION=1
-
 set -eu
 
-if [ $#=1 ]; then
-  _APP_ID=$1
-else
-  echo "run setup_deploy.sh <APPID>"
-  exit 1
-fi
+usage_exit() {
+        echo "Usage: $0 [-t {mapray | ui} ] [-a {dev | staging | production}] " 1>&2
+        exit 1
+}
 
+_TARGET="null"
+_APP_ID="null"
+
+while getopts :t:a: OPT
+do
+    case $OPT in
+        t)  _TARGET=$OPTARG
+            ;;
+        a)  _APP_ID=$OPTARG
+            ;;
+        h)  usage_exit
+            ;;
+        :|\?) usage_exit
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+[ "${_TARGET}" != "mapray" ] && [ "${_TARGET}" != "ui" ] && usage_exit
+[ "${_APP_ID}" != "dev" ] && [ "${_APP_ID}" != "staging" ] && [ "${_APP_ID}" != "production" ] && usage_exit
+
+echo "TARGET in setup_deploy:"${_TARGET}
 echo "APP ID in setup_deploy:"${_APP_ID}
 
+PACKAGE_ROOT=$(cd $(dirname $0)/../../packages/${_TARGET}; pwd)
+DIST="${PACKAGE_ROOT}/dist/umd/*"
+DEPLOY_DIR=$(cd $(dirname $0)/../public; pwd)
+
+if [ ${_TARGET} = "mapray" ]; then
+  PUBLIC=${DEPLOY_DIR}/mapray-js
+elif [ ${_TARGET} = "ui" ]; then
+  PUBLIC=${DEPLOY_DIR}/ui
+fi
+
+
+echo "PACKAGE_ROOT in setup_deploy:"${PACKAGE_ROOT}
+
 ## DEV to deplo
-cd ${HOME}
-VERSION=`node -pe "require('./package.json').version"` && \
-_NEXT_VERSION=`node -pe "require('semver').inc(\"${VERSION}\", '$1')"`
+VERSION=`node -pe "require('${PACKAGE_ROOT}/package.json').version"` && \
+echo "VERSION in setup_deploy:"${VERSION}
 
 # for Debug print
-
-
 if [ ${_APP_ID} = dev ]; then
     _NEXT_VERSION=`git describe --tags`
 elif [ ${_APP_ID} = staging ]; then
     _NEXT_VERSION=`git describe --tags --abbrev=0`
 elif [ ${_APP_ID} = production ]; then
-    _NEXT_VERSION=`node -pe "require('./package.json').version"`
+    _NEXT_VERSION=`node -pe "require('${PACKAGE_ROOT}/package.json').version"`
 else
     echo "build for production"
     exit 1
@@ -39,8 +64,11 @@ echo "NEXT VESRION is ${_NEXT_VERSION}"
 
 # copy
 mkdir -p ${PUBLIC}/v${_NEXT_VERSION} 2>/dev/null
-cp -r ${DIST}/mapray.js ${PUBLIC}/v${_NEXT_VERSION}/ || exit $?
-cp -r ${DIST}/maprayui.js ${PUBLIC}/v${_NEXT_VERSION}/ || exit $?
-cp -r ${DIST}/mapray.css ${DEPLOY_DIR}/styles/v${RESOURCE_VERSION}/ || exit $?
+cp -r ${DIST} ${PUBLIC}/v${_NEXT_VERSION}/ || exit $?
 
-echo "mapray-js file URL:./mapray-js/v${_NEXT_VERSION}/mapray.js"
+if [ ${_TARGET} = "ui" ]; then
+  RESOURCE_VERSION=1
+  cp -r ${PACKAGE_ROOT}/dist/mapray.css ${DEPLOY_DIR}/styles/v${RESOURCE_VERSION}/ || exit $?
+fi
+
+echo "coped dir URL:${PUBLIC}/v${_NEXT_VERSION}/"
--- _deploy/scripts/update_package_json.sh
+++ _deploy/scripts/update_package_json.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
-HOME=$(cd $(dirname $0)/../..; pwd)
 
 set -eu
 
-cd ${HOME}
-VERSION=`node -pe "require('./package.json').version"` && \
-NEXT_VERSION=`node -pe "require('semver').inc(\"${VERSION}\", '$1')"` && \
+usage_exit() {
+        echo "Usage: $0 [-t {mapray | ui} ] [-v patch|minor|major] " 1>&2
+        exit 1
+}
+
+_TARGET="null"
+_HOWTO_UPDATE="null"
+
+while getopts :t:a:v: OPT
+do
+    case $OPT in
+        t)  _TARGET=$OPTARG
+            ;;
+        v)  _HOWTO_UPDATE=$OPTARG
+            ;;
+        h)  usage_exit
+            ;;
+        :|\?) usage_exit
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+[ "${_TARGET}" != "mapray" ] && [ "${_TARGET}" != "ui" ] && usage_exit
+[ "${_HOWTO_UPDATE}" != "patch" ] && [ "${_HOWTO_UPDATE}" != "minor" ] && [ "${_HOWTO_UPDATE}" != "major" ] && usage_exit
+
+
+echo "TARGET in update_pacakge_json.sh:"${_TARGET}
+echo "HOW TO UPDATE in update_pacakge_json.sh:"${_HOWTO_UPDATE}
+PACKAGE_ROOT=$(cd $(dirname $0)/../../packages/${_TARGET}; pwd)
+
+VERSION=`node -pe "require('${PACKAGE_ROOT}/package.json').version"` && \
+NEXT_VERSION=`node -pe "require('semver').inc(\"${VERSION}\", '${_HOWTO_UPDATE}')"` && \
 echo ${NEXT_VERSION} && \
 node -e "\
-  var j = require('./package.json');\
+  var j = require('${PACKAGE_ROOT}/package.json');\
   j.version = \"${NEXT_VERSION}\";\
   var s = JSON.stringify(j, null, 2);\
-  require('fs').writeFileSync('./package.json', s);"
+  require('fs').writeFileSync('${PACKAGE_ROOT}/package.json', s);"
 
