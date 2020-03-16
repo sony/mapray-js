24964197593f4651efe8a44c729c69ba3d7a569c
Daisuke Matsumoto <Daisuke.Matsumoto@jp.sony.com>
2020-03-16 18:59:52 +0900

    add new command deploying to develop npm com when build except production

--- _deploy/scripts/deploy_to_npm_com.sh
+++ _deploy/scripts/deploy_to_npm_com.sh
@@ -3,19 +3,22 @@
 set -eu
 
 usage_exit() {
-        echo "Usage: $0 [-t {mapray | ui} ] [-n npm token] " 1>&2
+        echo "Usage: $0 [-t {mapray | ui} ] [-n npm token] [-d development flag] " 1>&2
         exit 1
 }
 
 _TARGET="null"
 _NPM_TOKEN="null"
+_DEV_FLAG=0
 
-while getopts :t:n: OPT
+while getopts :t:n:d OPT
 do
     case $OPT in
         t)  _TARGET=$OPTARG
             ;;
         n)  _NPM_TOKEN=$OPTARG
+            ;;
+        d)  _DEV_FLAG=1
             ;;
         h)  usage_exit
             ;;
@@ -40,6 +43,10 @@ echo "Command, yarn publish, is executed in "${PACKAGE_ROOT}
 #_VERSION=`git describe --tags --abbrev=0`
 _VERSION=`node -pe "require('${PACKAGE_ROOT}/package.json').version"`
 
+if [ ${_DEV_FLAG} = 1 ]; then
+  _VERSION=`git describe --tags`
+fi
+
 _NAME="null"
 
 if [ ${_TARGET} = "mapray" ]; then
@@ -48,8 +55,12 @@ elif [ ${_TARGET} = "ui" ]; then
   _NAME="ui-dummy"
 fi
 
+if [ ${_DEV_FLAG} = 1 ]; then
+  sed -i '' -e "s/@mapray\/${_NAME}/@mapray\/${_NAME}-dev/g" ./package.json
+fi
+
 cd ${PACKAGE_ROOT}
-echo "Will publish, yarn publish "mapray-${_NAME}-v${_VERSION}.tgz" version:"${_VERSION}
+echo "Will publish, yarn publish "mapray-${_NAME}-v${_VERSION}.tgz" version:"${_VERSION}" on `node -pe "require('${PACKAGE_ROOT}/package.json').name"`"
 yarn publish mapray-${_NAME}-v${_VERSION}.tgz --new-version ${_VERSION}
 
 
