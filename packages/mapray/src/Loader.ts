2d8da14d8dc0dc476246a6350230dba28ef146c4
masa-sony <Masaru.Okaya@sony.com>
2021-06-16 22:06:45 +0900

    typescript: Loader, SceneLoader, Resource, CredentialMode

--- packages/mapray/src/Loader.ts
+++ packages/mapray/src/Loader.ts
@@ -1,67 +1,71 @@
+import Scene from "./Scene";
+import Entity from "./Entity";
+import CredentialMode from "./CredentialMode";
 import Resource, { URLResource } from "./Resource";
 
 
 /**
- * @summary ローダークラス
- * @memberof mapray
+ * ローダークラス
  */
-class Loader {
+abstract class Loader {
+
+    protected _scene: Scene;
+
+    protected _resource: Resource;
+
+    private _status: Loader.Status;
+
+    private _onLoad: Loader.FinishCallback;
+
+    protected _onEntity: Loader.EntityCallback;
 
     /**
-     * @param {mapray.Scene} scene 読み込み先のシーン
-     * @param {mapray.Resource} resource 
-     * @param {object} [options={}]
-     * @param {object} [options.onLoad] 全ての読み込み完了時に呼ばれる
-     * @param {mapray.Loader.EntityCallback} [options.onEntity] エンティティが読み込まれるたびに呼ばれる
+     * @param scene 読み込み先のシーン
+     * @param resource
+     * @param options
      */
-    constructor( scene, resource, options={} ) {
+    constructor( scene: Scene, resource: Resource, options: Loader.Option = {} ) {
         this._scene = scene;
         if (!(resource instanceof Resource)) {
             throw new Error("Unsupported Resource Type: " + resource);
         }
         this._resource = resource;
         this._status = Loader.Status.NOT_LOADED;
-        this._onLoad = options.onLoad || defaultOnLoadCallback;
-        this._onEntity = options.onEntity || defaultOnEntityCallback;
+        this._onLoad = options.onLoad || Loader.defaultOnLoadCallback;
+        this._onEntity = options.onEntity || Loader.defaultOnEntityCallback;
     }
 
 
     /**
-     * @summary 読み込み先のシーン
-     * @type {mapray.Scene}
-     * @readonly
+     * 読み込み先のシーン
      */
-    get scene() { return this._scene; }
+    get scene(): Scene { return this._scene; }
 
 
     /**
-     * @summary シーンリソース
-     * @type {string}
-     * @readonly
+     * シーンリソース
      */
-    get resource() { return this._resource; }
+    get resource(): Resource { return this._resource; }
 
 
     /**
      * ローダー読み込みの状態
-     * @readonly
      */
-    get status() { return this._status; }
+    get status(): Loader.Status { return this._status; }
 
 
     /**
-     * @private
+     *
      */
-    _setStatus( status ) {
+    private _setStatus( status: Loader.Status ) {
         this._status = status;
     }
 
 
     /**
-     * @summary 読み込みを実行します。
-     * @returns {Promise}
+     * 読み込みを実行します。
      */
-    load()
+    load(): Promise<void>
     {
         if ( this.status !== Loader.Status.NOT_LOADED ) {
             return Promise.reject( new Error( "Illegal Status: " + this.status ) );
@@ -101,18 +105,15 @@ class Loader {
 
 
     /**
-     * @summary 読み込み処理の実態。継承クラスによって実装される。
-     * @private
+     * 読み込み処理の実態。継承クラスによって実装される。
      */
-    _load() {
-        throw new Error( "_load() is not implemented in " + this.constructor.name );
-    }
+    protected abstract _load(): Promise<void>;
 
 
     /**
-     * @summary 読み込みの取り消し
-     * @desc
-     * <p>終了コールバック関数は isSuccess == false で呼び出される。</p>
+     * 読み込みの取り消し
+     *
+     * 終了コールバック関数は `isSuccess == false` で呼び出される。
      */
     cancel()
     {
@@ -127,19 +128,17 @@ class Loader {
 
 
     /**
-     * @summary キャンセル時に行う処理。継承クラスによって実装される。
-     * @private
+     * キャンセル時に行う処理。継承クラスによって実装される。
      */
-    _cancel()
+    protected _cancel()
     {
     }
 
 
     /**
      * 取り消し状態のとき例外を投げる
-     * @private
      */
-    _check_cancel()
+    protected _check_cancel()
     {
         if ( this.status === Loader.Status.CANCELED ) {
             throw new Error( "canceled" );
@@ -148,21 +147,35 @@ class Loader {
 }
 
 
+
+namespace Loader {
+
+
+
+export interface Option {
+    /**
+     * 全ての読み込み完了時に呼ばれる
+     */
+    onLoad?: FinishCallback,
+
+    /**
+     * エンティティが読み込まれるたびに呼ばれる
+     */
+    onEntity?: Loader.EntityCallback,
+}
+
+
+
 /**
- * @summary Entity読み込みコールバック
- * @callback EntityCallback
- * @desc
- * <p>読み込み処理の中でEntityが生成される際に呼ばれる。
+ * Entity読み込みコールバック
+ *
+ * 読み込み処理の中でEntityが生成される際に呼ばれる。
  * 一度の読み込み(load()呼び出し)において複数のエンティティが生成される場合は、エンティティが生成されるたびに呼ばれる。
  * この関数をLoaderに指定する場合は、callback処理の中でEntityをsceneへ追加する必要がある。
  * geojsonのように、要素ごとにプロパティを含められるような場合は、propにより値にアクセスする。
- * </p>
- *
- * @param  {mapray.Loader} loader Loader
- * @param  {mapray.Entity} entity 読み込まれたEntity
- * @param  {object} prop エンティティ生成の元となるオブジェクト
  *
  * @example
+ * ```ts
  * const loader = new mapray.SceneLoader( viewer.scene, resource, {
  *         onEntity: ( loader, entity, prop ) => {
  *             entity.setScale( [ 2, 2, 2 ] );
@@ -170,65 +183,52 @@ class Loader {
  *         }
  * } );
  * loader.load();
- * 
- * @memberof mapray.Loader
+ * ```
+ *
+ * @param  loader Loader
+ * @param  entity 読み込まれたEntity
+ * @param  prop エンティティ生成の元となるオブジェクト
  */
+export type EntityCallback = ( loader: Loader, entity: Entity, prop: object ) => void;
 
 
 
-Loader.Status = {
-    NOT_LOADED : "Not Loaded",
-    LOADING    : "Loading",
-    LOADED     : "Loaded",
-    CANCELED   : "Canceled",
-    ERROR      : "ERROR"
+/**
+ * 終了コールバック
+ *
+ * シーンの読み込みが終了したときに呼び出される関数の型である。
+ *
+ * @param loader     読み込みを実行したローダー
+ * @param isSuccess  成功したとき true, 失敗したとき false
+ */
+export type FinishCallback = ( loader: Loader, isSuccess: boolean ) => void;
+
+
+
+export enum Status {
+    NOT_LOADED,
+    LOADING,
+    LOADED,
+    CANCELED,
+    ERROR,
+    ABORTED,
 };
 
-function defaultOnLoadCallback( loader, isSuccess )
+
+export function defaultOnLoadCallback( loader: Loader, isSuccess: boolean ): void
 {
 }
 
-function defaultOnEntityCallback( loader, entity )
+
+export function defaultOnEntityCallback( loader: Loader, entity: Entity, prop: object ): void
 {
     loader.scene.addEntity( entity );
 }
 
 
-/**
- * @summary リソース要求変換関数
- * @callback TransformCallback
- * @desc
- * <p>リソースのリクエスト時に URL などを変換する関数の型である。</p>
- *
- * @param  {string}                          url   変換前のリソース URL
- * @param  {mapray.Resource.ResourceType} type  リソースの種類
- * @return {mapray.Loader.TransformResult}    変換結果を表すオブジェクト
- *
- * @example
- * function( url, type ) {
- *     return {
- *         url:         url,
- *         credentials: mapray.CredentialMode.SAME_ORIGIN,
- *         headers: {
- *             'Header-Name': 'Header-Value'
- *         }
- *     };
- * }
- *
- * @memberof mapray.Loader
- */
 
+} // namespace Loader
 
-/**
- * @summary リソース要求変換関数の変換結果
- * @typedef {object} TransformResult
- * @desc
- * <p>関数型 {@link mapray.Loader.TransformCallback} の戻り値のオブジェクト構造である。</p>
- * @property {string}                url                 変換後のリソース URL
- * @property {mapray.CredentialMode} [credentials=SAME_ORIGIN]  クレデンシャルモード
- * @property {object}                [headers={}]        リクエストに追加するヘッダーの辞書 (キーがヘッダー名、値がヘッダー値)
- * @memberof mapray.Loader
- */
 
 
 export default Loader;
--- packages/mapray/src/Resource.ts
+++ packages/mapray/src/Resource.ts
@@ -5,178 +5,287 @@ import CredentialMode from "./CredentialMode";
 
 
 /**
- * @classdesc リソースクラス
+ * リソースを表現するクラスです。
  * URLやDB、クラウドサービス等、各種リソースへのアクセスを同一インターフェースで提供することを目的とした抽象クラスです。
- * 基本機能：
- * ・コンストラクタ等によりあらかじめURLやデータの位置を示すプロパティを設定
- * ・load()によりリソースを読み込む
- * ・loadSubResource()によりサブリソースを読み込む
  *
- * @memberof mapray
+ * 基本機能：
+ * - コンストラクタ等によりあらかじめURLやデータの位置を示すプロパティを設定
+ * - load()によりリソースを読み込む
+ * - loadSubResource()によりサブリソースを読み込む
  */
-class Resource {
+abstract class Resource {
 
     /**
      * リソースを読み込みます。
      */
-    async load( options={} ) {
+    async load( options: Resource.Option = {} ): Promise<any> {
         throw new Error( "Not Implemented" );
     }
 
     /**
-     * @summary リソースの読み込みをキャンセルできる場合はキャンセルします。
+     * リソースの読み込みをキャンセルできる場合はキャンセルします。
      */
     cancel() {
     }
 
     /**
-     * @summary サブリソースをサポートするかを返します。
-     * @return {boolean}
+     * サブリソースをサポートするかを返します。
      */
-    loadSubResourceSupported() {
+    loadSubResourceSupported(): boolean {
         return false;
     }
 
     /**
-     * @summary サブリソースを読み込みます。
-     * @param {string}  url       URL
-     * @param {object} options
-     * @param {mapray.Resource.ResourceType} [options.type] 返却するタイプを指定します。
-     * @return {object} options.type に応じた型で返却されます。
+     * サブリソースを読み込みます。
+     * @param url     URL
+     * @param options
+     * @return `options.type` に応じた型で返却されます。
      */
-    async loadSubResource( url, options={} ) {
+    async loadSubResource( url: string, options: Resource.Option = {} ): Promise<any> {
         throw new Error( "Not Supported" );
     }
 
     /**
-     * @summary 関連リソースをサポートするかを返します。
-     * @return {boolean}
+     * 関連リソースをサポートするかを返します。
      */
-    resolveResourceSupported() {
+    resolveResourceSupported(): boolean {
       return false;
     }
 
     /**
-     * @summary 関連リソースを読み込みます。
-     * @return {boolean}
+     * 関連リソースを読み込みます。
      */
-    async resolveResource( url ) {
+    resolveResource( url: string ): Resource {
         throw new Error( "Not Supported" );
     }
+}
+
+
+namespace Resource {
+
+
+export interface Option {
+    /**
+     * 返却するタイプを指定します。
+     */
+    type?: Resource.Type;
 
     /**
-     * @summary リソース型
+     *
      */
-    static get ResourceType() { return ResourceType; }
+    transform?: Resource.TransformCallback;
+
+    /**
+     *
+     */
+    signal?: AbortSignal;
 }
 
 
 
 /**
- * @summary リソースの種類
- * @enum {object}
- * @memberof mapray.ResourceType
- * @constant
+ * リソース要求変換関数
+ *
+ * リソースのリクエスト時に URL などを変換する関数の型である。
+ *
+ * ```ts
+ * function( url, type ) {
+ *     return {
+ *         url:         url,
+ *         credentials: mapray.CredentialMode.SAME_ORIGIN,
+ *         headers: {
+ *             'Header-Name': 'Header-Value'
+ *         }
+ *     };
+ * }
+ *
+ * @param  url   変換前のリソース URL
+ * @param  type  リソースの種類
+ * @return       変換結果を表すオブジェクト
  */
-const ResourceType = {
-
-    /**
-     * JSON
-     */
-    JSON: { id: "JSON" },
-
-    /**
-     * バイナリ(ArrayBuffer)
-     */
-    BINARY: { id: "BINARY" },
-
-    /**
-     * 画像（Image）
-     */
-    IMAGE: { id: "IMAGE" }
-
-};
+export type TransformCallback = ( url: string, type: any ) => Resource.TransformResult;
 
 
 
 /**
- * @classdesc URLリソースです。
+ * リソース要求変換関数の変換結果
+ *
+ * 関数型 [[Loader.TransformCallback]] の戻り値のオブジェクト構造である。
+ */
+export interface TransformResult {
+    /**
+     * 変換後のリソース URL
+     */
+    url: string;
+
+    /**
+     * クレデンシャルモード
+     */
+    credentials?: CredentialMode;
+
+    /**
+     * リクエストに追加するヘッダーの辞書 (キーがヘッダー名、値がヘッダー値)
+     */
+    headers?: object;
+}
+
+
+
+export function defaultTransformCallback( url: string, type: Resource.Type ) {
+    return { url: url };
+}
+
+
+
+/**
+ * リソースの種類
+ */
+export enum Type {
+
+    /**
+     * JSON
+     */
+    JSON,
+
+    /**
+     * バイナリ(ArrayBuffer)
+     */
+    BINARY,
+
+    /**
+     * 画像（Image）
+     */
+    IMAGE,
+
+    /**
+     * テキスト
+     */
+    TEXT,
+};
+
+
+
+}
+// namespace Resource
+
+
+
+/**
+ * HTTPサーバなどで配信されるリソースを表現します。
+ * ベースとなるリソースがサブリソースを持つようなリソースを想定したクラスです。
+ *
+ * 例としてhtmlの場合について説明します。
+ * htmlファイルは画像やcssなど様々なサブリソースを持つことができます。
+ * htmlファイルをベースとなるリソースと呼び、それ以外のファイルをサブリソースと呼びます。
+ * サブリソースはベースとなるリソースからの相対パスで表現されることや、絶対パスで表現されることがあります。
+ * また、htmlファイルには、別のhtmlファイルへのリンクが記述されることがあり、別のhtmlファイルはさらに画像やcssなどのファイルを持ちます。
+ * このクラスは上記のような状況を表現することができます。
+ * - [[load]] はベースとなるリソースの内容を読み込みます。
+ * - [[loadSubResource]] は、ベースとなるリソースからの相対パスを指定してサブリソースの内容を読み込みます（絶対パスを指定すると指定したURLへ直接アクセスします）。
+ * - [[resolveResource]] は、別のhtmlファイルへのリンクをたどるような状況を表現する場合に利用します。相対パスや絶対パスを指定すると、新たな Resource を得ることができます。
+ *
+ * 下記に使用例を示します。
+ * ```ts
+ * // https://.../test.html を起点とするリソースを定義します。
+ * const resource = new URLResource("https://.../test.html");
+ * // test.html の内容を読み込みます。
+ *
+ * const html = await resource.load({ type: Resource.ResourceType.TEXT });
+ * // test.html と同一パスにある画像を読み込みます。
+ *
+ * const image = await resource.loadSubResource("image.png",
+ *   { type: Resource.ResourceType.IMAGE });
+ *
+ * // test.html からの相対パス(sub) にある 別のhtml(other.html) を起点とするリソースを定義します。
+ * const otherResource = resource.resolveResource("sub/other.html");
+ *
+ * // other.htmlの内容を読み込みます。
+ * const otherHtml = await otherResource.load({ type: Resource.ResourceType.TEXT });
+ *
+ * // other.htmlと同一パスにあるの画像を読み込みます。
+ * const otherImage = await otherResource.loadSubResource("image.png",
+ *   { type: Resource.ResourceType.IMAGE });
+ * ```
  */
 class URLResource extends Resource {
 
+    private _url: string;
+
+    private _base_url: string;
+
+    private _type: Resource.Type;
+
+    private _transform: Resource.TransformCallback;
+
+    private _abort_ctrl: AbortController;
+
     /**
-     * @param {string} url
-     * @param {object} [options]
-     * @param {mapray.Resource.ResourceType} [options.type]
-     * @param {mapray.Transform} [options.transform]
+     * @param url
+     * @param options
      */
-    constructor( url, options={} ) {
+    constructor( url: string, options: Resource.Option = {} ) {
         super();
         this._url = url;
         const index = url.lastIndexOf( '/' );
         if ( index === -1 ) throw new Error( "invalid url" );
         this._base_url = this._url.substr( 0, index + 1 );
-        this._type = options.type || "json";
-        this._transform = options.transform || defaultTransformCallback;
+        this._type = options.type || Resource.Type.JSON;
+        this._transform = options.transform || Resource.defaultTransformCallback;
         this._abort_ctrl = new AbortController();
     }
 
     /**
-     * @summary リソースのurl
-     * @type {string}
+     * リソースのurl
      */
-    get url() {
+    get url(): string {
         return this._url;
     }
 
     /**
-     * @summary このリソースを読み込みます。
-     * @param {object} [options]
+     * このリソースを読み込みます。
+     * @param options
      */
-    async load( options={} ) {
+    async load( options: Resource.Option = {} ): Promise<any> {
         return await this._loadURLResource( this._url, options.type || this._type, options );
     }
 
     /**
-     * @summary リソースの読み込みをキャンセルします。
+     * リソースの読み込みをキャンセルします。
      */
     cancel() {
         this._abort_ctrl.abort();
     }
 
     /**
-     * @summary このクラスでのデフォルト実装では、trueを返却します。
-     * @return {boolean}
+     * このクラスでのデフォルト実装では、trueを返却します。
      */
-    loadSubResourceSupported() {
+    loadSubResourceSupported(): boolean {
         return true;
     }
 
     /**
-     * @summary サブリソースを読み込みます。
-     * @param {string} subUrl URL
-     * @param {object} options
-     * @param {mapray.Resource.ResourceType} [options.type] 返却するタイプを指定します。
-     * @return {object} options.type に応じた型で返却されます。
+     * サブリソースを読み込みます。
+     * @param subUrl URL
+     * @param options
+     * @return `options.type` に応じた型で返却されます。
      */
-    async loadSubResource( subUrl, options={} ) {
+    async loadSubResource( subUrl: string, options: Resource.Option = {} ): Promise<any> {
         return await this._loadURLResource( Dom.resolveUrl( this._base_url, subUrl ), options.type, options );
     }
 
     /**
-     * @summary 関連リソースをサポートするかを返します。
-     * @return {boolean}
+     * 関連リソースをサポートするかを返します。
      */
-    resolveResourceSupported() {
+    resolveResourceSupported(): boolean {
       return true;
     }
 
     /**
-     * @summary 関連リソースを読み込みます。
-     * @param {string} url　
-     * @return {Resource}
+     * 関連リソースを読み込みます。
+     * @param sub_url
+     * @return
      */
-    resolveResource( sub_url ) {
+    resolveResource( sub_url: string ): Resource {
         const url = Dom.resolveUrl( this._base_url, sub_url );
         return new URLResource( url, {
                 transform: this._transform
@@ -185,39 +294,39 @@ class URLResource extends Resource {
 
 
     /**
-     * @param {string} url
-     * @param {mapray.Resource.ResourceType} [type]
-     * @private
+     * @param url
+     * @param type
      */
-    async _loadURLResource( url, type, options={} ) {
+    private async _loadURLResource( url: string, type?: Resource.Type, options: Resource.Option = {} ) {
         const tr = this._transform( url, type );
-        if ( type === ResourceType.IMAGE ) {
+        if ( type === Resource.Type.IMAGE ) {
             return await Dom.loadImage( tr.url, tr );
         }
         const http_option = this._make_fetch_params( tr ) || {};
         if ( options.signal ) http_option.signal = options.signal;
 
-        const response = await HTTP.get( tr.url, null, http_option );
+        const response = await HTTP.get( tr.url, undefined, http_option );
         if ( !response.ok ) throw new Error( response.statusText );
 
         return (
-            type === ResourceType.JSON ? await response.json():
-            type === ResourceType.BINARY ? await response.arrayBuffer():
+            type === Resource.Type.JSON   ? await response.json():
+            type === Resource.Type.TEXT   ? await response.text():
+            type === Resource.Type.BINARY ? await response.arrayBuffer():
             response
         );
     }
 
     /**
      * fetch() の init 引数に与えるオブジェクトを生成
-     * @private
      */
-    _make_fetch_params( tr ) {
-        var init = {
+    private _make_fetch_params( tr: Resource.TransformResult ): RequestInit {
+        var init: RequestInit = {
             signal:      this._abort_ctrl.signal,
-            credentials: (tr.credentials || CredentialMode.OMIT).credentials
+            credentials: tr.credentials || CredentialMode.OMIT,
         };
 
         if ( tr.headers ) {
+            // @ts-ignore
             init.headers = tr.headers;
         }
 
@@ -227,11 +336,5 @@ class URLResource extends Resource {
 
 
 
-function defaultTransformCallback( url, type ) {
-    return { url: url };
-}
-
-
-
-export { URLResource, ResourceType };
+export { URLResource };
 export default Resource;
