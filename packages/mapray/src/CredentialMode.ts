9ccd0dab3601cde208bbce2e5c52e9307cfaef24
masa-sony <Masaru.Okaya@sony.com>
2021-06-16 22:06:45 +0900

    typescript: Loader, SceneLoader, Resource, CredentialMode

--- packages/mapray/src/CredentialMode.ts
+++ packages/mapray/src/CredentialMode.ts
@@ -1,29 +1,26 @@
 /**
- * @summary クレデンシャルモード
- * @desc
- * <p>HTTP リクエストのクレデンシャルモードを表現する型である。<p>
- * @enum {object}
- * @memberof mapray
- * @constant
+ * クレデンシャルモード
+ *
+ * HTTP リクエストのクレデンシャルモードを表現する型である。
  * @see https://developer.mozilla.org/docs/Web/API/Request/credentials
- * @see mapray.StandardDemProvider
+ * @see [[mapray.StandardDemProvider]]
  */
-var CredentialMode = {
+enum CredentialMode {
 
     /**
      * 決してクッキーを送信しない
      */
-    OMIT: { id: "OMIT", credentials: "omit" },
+    OMIT = "omit",
 
     /**
      * URL が呼び出し元のスクリプトと同一オリジンだった場合のみ、クッキーを送信
      */
-    SAME_ORIGIN: { id: "SAME_ORIGIN", credentials: "same-origin" },
+    SAME_ORIGIN = "same-origin",
 
     /**
      * クロスオリジンの呼び出しであっても、常にクッキーを送信
      */
-    INCLUDE: { id: "INCLUDE", credentials: "include" }
+    INCLUDE = "include",
 
 };
 
