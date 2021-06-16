50a211c9ab39bc1a26d3580cc4814201e5400b18
masa-sony <Masaru.Okaya@sony.com>
2021-06-16 21:25:53 +0900

    typescript: GeoMath, GeoPoint, Orientation, GeoRegion

--- packages/mapray/src/GeoRegion.ts
+++ packages/mapray/src/GeoRegion.ts
@@ -2,52 +2,51 @@ import GeoMath from "./GeoMath";
 import GeoPoint from "./GeoPoint";
 
 /**
- * @summary 地理空間的Region
- *
- * @memberof mapray
+ * 地理空間的Region
  */
 class GeoRegion {
+
+    private _empty: boolean = true;
+
+    private _min_lon: number = 0;
+    private _max_lon: number = 0;
+    private _min_lat: number = 0;
+    private _max_lat: number = 0;
+    private _min_alt: number = 0;
+    private _max_alt: number = 0;
 
     /**
      * 初期値は empty である。
      * lon は次の条件とする。
-     * <p> 条件 this._min_lon < this._max_lon </p>
-     * <p> 条件 this._max_lon - this._min_lon <= 360 </p>
+     * - this._min_lon < this._max_lon
+     * - this._max_lon - this._min_lon <= 360
      */
     constructor()
     {
-        this._empty = true;
-        this._min_lon = 0;
-        this._max_lon = 0;
-        this._min_lat = 0;
-        this._max_lat = 0;
-        this._min_alt = 0;
-        this._max_alt = 0;
     }
 
 
     /**
-     * @summary 空確認
+     * 空確認
      *
-     * @return {boolean}        empty値
+     * @return  empty値
      */
-    empty()
+    empty(): boolean
     {
         return this._empty;
     }
 
 
     /**
-     * @summary Pointを追加
-     * <p> 条件 this._min_lon < this._max_lon </p>
-     * <p> 条件 this._max_lon - this._min_lon <= 360 </p>
+     * Pointを追加
+     * - 条件 this._min_lon < this._max_lon </p>
+     * - 条件 this._max_lon - this._min_lon <= 360 </p>
      *
-     * @param {number}  lon  地理空間位置 longitude
-     * @param {number}  lat  地理空間位置 latitude
-     * @param {number}  alt  地理空間位置 altitude
-     * @private
+     * @param lon  地理空間位置 longitude
+     * @param lat  地理空間位置 latitude
+     * @param alt  地理空間位置 altitude
      */
-    _add( lon, lat, alt )
+    private _add( lon: number, lat: number, alt: number )
     {
         if ( this._empty ) {
             this._empty = false;
@@ -86,14 +85,13 @@ class GeoRegion {
     }
 
     /**
-     * @summary base_lon より右となる最小の longitude を算出
+     * base_lon より右となる最小の longitude を算出
      *
-     * @param {number}  base_lon  基準となるlongitude
-     * @param {number}  lon       確認したいlongitude
-     * @return {number} 最小longitude
-     * @private
+     * @param  base_lon  基準となるlongitude
+     * @param  lon       確認したいlongitude
+     * @return 最小longitude
      */
-     _calcRightPosition( base_lon, lon )
+     private _calcRightPosition( base_lon: number, lon: number ): number
      {
          let diff = lon - base_lon;
          diff = diff - 360 * Math.floor(diff / 360);
@@ -102,22 +100,22 @@ class GeoRegion {
 
 
     /**
-     * @summary GeoPointを追加
+     * GeoPointを追加
      *
-     * @param {mapary.GeoPoint}  point  地理空間位置
+     * @param  point  地理空間位置
      */
-    addPoint( point )
+    addPoint( point: GeoPoint )
     {
         this._add( point.longitude, point.latitude, point.altitude );
     }
 
 
     /**
-     * @summary GeoPointを追加
+     * GeoPointを追加
      *
-     * @param {...mapary.GeoPoint}  points  地理空間位置
+     * @param  points  地理空間位置
      */
-    addPoints( ...points )
+    addPoints( ...points: GeoPoint[] )
     {
         points.forEach( point => {
             this._add( point.longitude, point.latitude, point.altitude );
@@ -126,11 +124,11 @@ class GeoRegion {
 
 
     /**
-     * @summary PointArrayを追加
+     * PointArrayを追加
      *
-     * @param {number[]}  pointsArray  地理空間位置Array
+     * @param  pointsArray  地理空間位置Array
      */
-    addPointsAsArray( pointsArray )
+    addPointsAsArray( pointsArray: number[] | Float64Array )
     {
         for ( let i = 0; i < pointsArray.length; i += 3 ) {
             this._add( pointsArray[i], pointsArray[i+1], pointsArray[i+2] );
@@ -139,11 +137,11 @@ class GeoRegion {
 
 
     /**
-     * @summary GeoRegionを結合
+     * GeoRegionを結合
      *
-     * @param {mapray.GeoRegion}  region  地理空間Region
+     * @param region  地理空間Region
      */
-    merge( region )
+    merge( region: GeoRegion )
     {
         if( region.empty() ) { return; }
         if ( this._empty ) {
@@ -220,21 +218,21 @@ class GeoRegion {
 
 
     /**
-     * @summary Region内の任意点の取得
-     * center     ( 0.5, 0.5 )
-     * east       (   1, 0.5 )
-     * west       (   0, 0.5 )
-     * north      ( 0.5,   1 )
-     * south      ( 0.5,   0 )
-     * northEast  (   1,   1 )
-     * southWest  (   0,   0 )
+     * Region内の任意点の取得
+     * - center     ( 0.5, 0.5 )
+     * - east       (   1, 0.5 )
+     * - west       (   0, 0.5 )
+     * - north      ( 0.5,   1 )
+     * - south      ( 0.5,   0 )
+     * - northEast  (   1,   1 )
+     * - southWest  (   0,   0 )
      *
-     * @param {number}  lon_pos   longitude位置割合
-     * @param {number}  lat_pos   latitude位置割合
-     * @param {number}  [alt_pos] anlitude位置割合
-     * @return {mapary.GeoPoint}   GeoPoint
+     * @param lon_pos  longitude位置割合
+     * @param lat_pos  latitude位置割合
+     * @param alt_pos  anlitude位置割合
+     * @return   GeoPoint
      */
-    getPoint( lon_pos, lat_pos, alt_pos=0 )
+    getPoint( lon_pos: number, lat_pos: number, alt_pos: number = 0 ): GeoPoint | null
     {
         if ( this._empty ) { return null; }
         return new GeoPoint(
@@ -246,11 +244,11 @@ class GeoRegion {
 
 
     /**
-     * @summary SouthWestのGeopointを返却
+     * SouthWestのGeopointを返却
      *
-     * @return {mapary.GeoPoint}   南西(min)のGeoPoint
+     * @return  南西(min)のGeoPoint
      */
-    getSouthWest()
+    getSouthWest(): GeoPoint | null
     {
         if ( this._empty ) { return null; }
         return new GeoPoint( this._min_lon, this._min_lat, this._min_alt );
@@ -258,11 +256,11 @@ class GeoRegion {
 
 
     /**
-     * @summary NorthEastのGeopointを返却
+     * NorthEastのGeopointを返却
      *
-     * @return {mapary.GeoPoint}  北東(max)のGeoPoint
+     * @return  北東(max)のGeoPoint
      */
-    getNorthEast()
+    getNorthEast(): GeoPoint | null
     {
         if ( this._empty ) { return null; }
         return new GeoPoint( this._max_lon, this._max_lat, this._max_alt );
@@ -270,11 +268,11 @@ class GeoRegion {
 
 
     /**
-     * @summary 中心位置のGeopointを返却
+     * 中心位置のGeopointを返却
      *
-     * @return {mapary.GeoPoint}  中心(center)のGeoPoint
+     * @return  中心(center)のGeoPoint
      */
-    getCenter()
+    getCenter(): GeoPoint | null
     {
         if ( this._empty ) { return null; }
         return this.getPoint( 0.5, 0.5, 0.5 );
@@ -282,26 +280,28 @@ class GeoRegion {
 
 
     /**
-     * @summary RegionのLongitude方向 の地表面距離を算出
+     * RegionのLongitude方向 の地表面距離を算出
      *
-     * @return {number}  Longitude方向の地表面距離
+     * @return  Longitude方向の地表面距離
      */
-    getLongitudeDistance()
+    getLongitudeDistance(): number | null
     {
-        if ( this._empty ) { return null; }
-        return this.getPoint( 0, 0.5 ).getGeographicalDistance( this.getPoint( 1, 0.5 ) );
+        const a = this.getPoint( 0, 0.5 );
+        const b = this.getPoint( 1, 0.5 );
+        return (a && b) ? a.getGeographicalDistance( b ) : null;
     }
 
 
     /**
-     * @summary RegionのLatitude方向 の地表面距離を算出
+     * RegionのLatitude方向 の地表面距離を算出
      *
-     * @return {number}  Latitude方向の地表面距離
+     * @return  Latitude方向の地表面距離
      */
-    getLatitudeDistance()
+    getLatitudeDistance(): number | null
     {
-        if ( this._empty ) { return null; }
-        return this.getPoint( 0.5, 0 ).getGeographicalDistance( this.getPoint( 0.5, 1 ) );
+        const a = this.getPoint( 0.5, 0 );
+        const b = this.getPoint( 0.5, 1 );
+        return (a && b) ? a.getGeographicalDistance( b ) : null;
     }
 }
 
