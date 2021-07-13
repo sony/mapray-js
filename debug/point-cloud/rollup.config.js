4756bbfa6133b5b913990e77cdf749e538bab062
masa-sony <Masaru.Okaya@sony.com>
2021-07-14 01:08:01 +0900

    typescript: debug: update point-cloud

--- debug/point-cloud/rollup.config.js
+++ debug/point-cloud/rollup.config.js
@@ -1,216 +1,81 @@
-import path from 'path'
-import { terser } from 'rollup-plugin-terser'
-import babel from 'rollup-plugin-babel'
-import { string } from 'rollup-plugin-string'
-import replace from 'rollup-plugin-replace'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import preprocess from 'rollup-plugin-preprocess'
-import commonjs from 'rollup-plugin-commonjs'
-import fs from 'fs';
-//import { getSourcePaths, addLocalSettings, makeExternalPredicate } from '../rollup.config.local.js'
-import pkg from './package.json'
-
-
-//>>> ../rollup.config.local.js
-const localConfig = (appDir) => (
-        [
-            replace({
-                '"@mapray/mapray-js"': JSON.stringify(appDir+"../node_modules/@mapray/mapray-js/src/index.js"),
-                delimiters: ['', ''],
-                exclude: appDir+"../packages/"
-            }),
-            replace({
-                '"@mapray/ui"': JSON.stringify(appDir+"../node_modules/@mapray/ui/src/index.js"),
-                delimiters: ['', ''],
-                exclude: appDir+"../packages/"
-            })
-        ]
-)
-
-function getSourcePaths(appDir) {
-    const mapray_js = fs.realpathSync(appDir + "../node_modules/@mapray/mapray-js");
-    const ui        = fs.realpathSync(appDir + "../node_modules/@mapray/ui");
-    return [
-        mapray_js + "/src/**/*.js",
-        mapray_js + "/src/**/*.vert",
-        mapray_js + "/src/**/*.frag",
-        ui + "/src/**/*.js",
-    ];
-}
-
-function addLocalSettings(env, appDir, bundle) {
-    let plugins = bundle.plugins.concat() // copy
-    // should insert first replace method
-    let index = plugins.findIndex(item => JSON.stringify(item) === '{"name":"commonjs"}' ? true : false)
-    
-    if (index === -1) {
-        index = 0;
-    }
-
-    Array.prototype.splice.apply(plugins, [index, 0].concat(localConfig(appDir)))
-    bundle.plugins = Object.assign(bundle.plugins, plugins)
-
-    return bundle;
-};
-
-function makeExternalPredicate(externalArr) {
-    if (externalArr.length === 0) {
-        return () => false
-    }
-    const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`)
-    return id => pattern.test(id)
-}
-//<<< ../rollup.config.local.js
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+import { string } from 'rollup-plugin-string';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import preprocess from 'rollup-plugin-preprocess';
+// import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 
 
-var outdir = "dist/"
-
-const appDir = path.join(__dirname, '../')
+const outdir = "dist/";
 
 
-let accessToken = '"<your access token here>"'
-if (process.env.MAPRAY_ACCESS_TOKEN)  {
-    accessToken = JSON.stringify(process.env.MAPRAY_ACCESS_TOKEN)
-}
 
-let bingAccessToken = '"<your Bing Maps Key here>"'
-if (process.env.BINGMAP_ACCESS_TOKEN)  {
-    bingAccessToken = JSON.stringify(process.env.BINGMAP_ACCESS_TOKEN)
-}
+export default function() {
 
-const getPluginsConfig = (prod, format) => {
-    const params = [
-        postcss(),
-        replace({
-            '"<your access token here>"': accessToken,
-            delimiters: ['', '']
-        }),
-        replace({
-            '"<your Bing Maps Key here>"': bingAccessToken,
-            delimiters: ['', '']
-        }),
-        commonjs(),
-        resolve(),
-        preprocess({
-                include: (
-                    (process.env.local && format === "iife") ? [
-                        "src/**/*.js",
-                        ...getSourcePaths(appDir),
-                    ]:
-                    [
-                        "src/**/*.js"
-                    ]
-                ),
+    const isProd = process.env.BUILD === 'production';
+    const maprayAccessToken = process.env.MAPRAY_ACCESS_TOKEN;
+    const bingAccessToken = process.env.BINGMAP_ACCESS_TOKEN;
+
+    const bundle = {
+        input: 'src/index.ts',
+        output: {
+            file: outdir + 'bundle.js',
+            format: 'iife',
+            indent: false,
+            sourcemap: !isProd,
+        },
+        plugins: [
+            postcss(),
+            (maprayAccessToken ?
+                replace({
+                    '"<your access token here>"': JSON.stringify( maprayAccessToken ),
+                    delimiters: ['', ''],
+                }):
+                null
+            ),
+            (bingAccessToken ?
+                replace({
+                    '"<your Bing Maps Key here>"': JSON.stringify( bingAccessToken ),
+                    delimiters: ['', ''],
+                }):
+                null
+            ),
+            resolve(),
+            preprocess({
+                include: ([
+                    "src/**/*.js"
+                ]),
                 exclude: [], // disable default option (node_modules/**)
                 context: {
                     BUILD: process.env.BUILD,
                 }
-        }),
-        string({
-            include: [
-                '../../**/*.vert',
-                '../../**/*.frag',
-            ]
-        }),
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
-
-    if (prod) {
-        params.push(
-            terser({
-                compress: {
-                    unused: false,
-                    collapse_vars: false,
-                },
-                output: {
-                    comments: false,
-                },
-            })
-        )
+            }),
+            typescript({
+                tsconfig: './tsconfig.json',
+                tsconfigOverride: {
+                    compilerOptions: {
+                        sourceMap: true,
+                    }
+                }
+            }),
+            (isProd ?
+                terser({
+                    compress: {
+                        unused: false,
+                        collapse_vars: false,
+                    },
+                    output: {
+                        comments: false,
+                    },
+                }):
+                null
+            ),
+        ],
     }
 
-    return params;
-}
-
-const config = (build, format) => {
-    const bundle = {
-        input: 'src/index.js',
-        output: { 
-            file: `${outdir}bundle-${format}.js`,
-            format: format, 
-            indent: false,
-            sourcemap: true
-        }
-    }
-    bundle.plugins = getPluginsConfig(build, format)
     return bundle;
-}
-
-// get the setting when developing in local environment
-const loadLocalSetting = (env, format) => {
-    let bundle = config(env.BUILD, format)
-    if (format === "umd") {
-        bundle.external = makeExternalPredicate([
-                ...Object.keys(pkg.dependencies || {}),
-                ...Object.keys(pkg.peerDependencies || {})
-        ]);
-        bundle.globals = {
-            '@mapray/mapray-js': 'mapray',
-            '@mapray/ui': 'maprayui',
-        };
-    }
-    else if (format === "iife") {
-        bundle = addLocalSettings(env, appDir, bundle);
-    }
-    return bundle
-}
-
-const ADDITIONAL_SCRIPTS       = "<!--@ADDITIONAL_SCRIPTS@-->";
-const ADDITIONAL_SCRIPTS_START = "<!--@ADDITIONAL_SCRIPTS_START@-->";
-const ADDITIONAL_SCRIPTS_END   = "<!--@ADDITIONAL_SCRIPTS_END@-->";
-const ADDITIONAL_SCRIPTS_PATTERN = new RegExp(ADDITIONAL_SCRIPTS);
-const ADDITIONAL_SCRIPTS_START_END_PATTERN = new RegExp(ADDITIONAL_SCRIPTS_START + "([\\S\\s]*?)" + ADDITIONAL_SCRIPTS_END)
-
-const updateHTML = () => {
-    const htmlPath = "index-umd.html";
-    if (!fs.existsSync(htmlPath)) return;
-    let code = fs.readFileSync(htmlPath).toString();
-    if (code.match(ADDITIONAL_SCRIPTS_PATTERN)) { // current code is not local mode
-        if (process.env.local) {
-            code = code.replace(new RegExp(ADDITIONAL_SCRIPTS),
-                ADDITIONAL_SCRIPTS_START + "\n" +
-                "    <script src=\"../../node_modules/@mapray/mapray-js/dist/umd/mapray.js\"></script>\n" +
-                "    <script src=\"../../node_modules/@mapray/ui/dist/umd/maprayui.js\"></script>\n" +
-                "    " + ADDITIONAL_SCRIPTS_END
-            );
-            fs.writeFileSync(htmlPath, code);
-        }
-    }
-    else if (code.match(ADDITIONAL_SCRIPTS_START_END_PATTERN)) { // current code is local mode
-        if (!process.env.local) {
-            code = code.replace(ADDITIONAL_SCRIPTS_START_END_PATTERN, ADDITIONAL_SCRIPTS);
-            fs.writeFileSync(htmlPath, code);
-        }
-    }
-    else {
-        console.log("couldn't find additional scripts region: " + htmlPath);
-    }
-}
-
-module.exports = () => {
-    updateHTML();
-    if (process.env.local) {
-        return [
-          loadLocalSetting(process.env, "umd"),
-          loadLocalSetting(process.env, "iife"),
-        ]
-    }
-    return [
-      config(process.env.BUILD, "umd"),
-      config(process.env.BUILD, "iife"),
-    ];
 }
---
+++ debug/point-cloud/src/Option.ts
@@ -0,0 +1,321 @@
+
+
+
+class Option {
+
+    globalHandlers: Option.Handler<any>[];
+
+    map: Map<string, Option.PropertyProp<any>>;
+
+    constructor( registries: Option.Property<any>[] ) {
+        this.map = new Map();
+        this.globalHandlers = [];
+        if (registries) {
+            registries.forEach(registry => this.register(registry));
+        }
+    }
+
+    register<T>( property: Option.Property<T> ) {
+        const key = property.key; 
+        if (!key) throw new Error("?");
+        const type = property.type;
+        const handlers = property.handlers || [];
+        const value = property.value;
+        if ( Option.isSelectProperty( property ) ) {
+            const valueToOptionMap = new Map<T, Option.LabeledValueObject<T>>();
+            const optionOfValue = (value: T) => valueToOptionMap.get( value );
+            property.options.forEach( (option: T | Option.LabeledValueObject<T>) => {
+                    if ( Option.isLabeledValueObject(option) ) {
+                        valueToOptionMap.set( option.value, option );
+                    }
+                    else {
+                        valueToOptionMap.set( option, {
+                            label: `${option}`,
+                            value: option,
+                        });
+                    }
+            });
+            if (typeof(property.value) !== "undefined") {
+                if (!optionOfValue(property.value)) {
+                    throw new Error(`option not found: "${property.value}" in [${property.options.join(", ")}]`);
+                }
+            }
+            this.map.set( property.key, {
+                // @ts-ignore
+                key, type, value, handlers, valueToOptionMap, optionOfValue,
+                options: property.options, description: property.description,
+            });
+        }
+        else {
+            this.map.set( property.key, {
+                key, type, value, handlers,
+                description: property.description
+            });
+        }
+    }
+
+    set<T>( key: string, value: T ) {
+        const property = this.map.get(key);
+        if (!property) throw new Error("no property: " + key);
+        if ( Option.isSelectPropertyProp( property ) ) {
+            if (!property.optionOfValue(value)) {
+                throw new Error(`option not found: "${value}" in [${property.options.join(", ")}]`);
+            }
+        }
+        // @ts-ignore
+        property.value = value;
+        const event = { key, value };
+        property.handlers.forEach(handler => {
+                handler(event);
+        });
+        this.globalHandlers.forEach(handler => {
+                handler(event);
+        });
+    }
+
+    get( key: string ): any {
+        const property = this.map.get(key);
+        return property ? property.value : undefined;
+    }
+
+    getProperty( key: string ): Option.PropertyProp<any> {
+        const proeprty = this.map.get(key);
+        if ( !proeprty ) throw new Error("couldn't find " + key);
+        return proeprty;
+    }
+
+    onChangeAny( handler: Option.Handler<any> ) {
+        this.globalHandlers.push(handler);
+    }
+
+    onChange( key: string, handler: Option.Handler<any> ) {
+        const property = this.map.get(key);
+        if (!property) throw new Error("no property: " + key);
+        property.handlers.push(handler);
+    }
+}
+
+
+interface ButtonParam {
+    class?: string;
+    onclick?: (button: HTMLElement, event: MouseEvent) => void;
+}
+
+interface CheckboxParam {
+    class?: string;
+    initialValue?: boolean;
+    onchange?: (value: boolean, event: Event) => void;
+    onui?: (ui: HTMLInputElement) => void;
+}
+
+interface SelectParam<T> {
+    class?: string;
+    initialValue?: string;
+    onchange?: (value: T, event: Event) => void;
+    onui?: (ui: HTMLSelectElement) => void;
+    label?: string;
+}
+
+
+
+class DomTool {
+    static createButton(name: string, options: ButtonParam = {}) {
+        const button = document.createElement("button");
+        if (options.class) button.setAttribute("class", options.class);
+        button.innerHTML = name;
+        const onclick = options.onclick;
+        if (onclick) {
+            button.onclick = event => {
+                onclick(button, event);
+            };
+        }
+        return button;
+    }
+
+    static createSelect<T>(items: (T|Option.LabeledValueObject<T>)[], options: SelectParam<T> ) {
+        const props = {
+            mapDomValueToValue: {
+            }
+        };
+        const mapDomValueToValue = new Map();
+        const select = items.reduce<HTMLSelectElement>((select, item) => {
+                const option = document.createElement("option");
+                if (typeof(item) === "object") {
+                    // @ts-ignore
+                    option.innerHTML = item.label;
+                    // @ts-ignore
+                    option.value     = item.domValue || item.value.toString();
+                    // @ts-ignore
+                    mapDomValueToValue.set(option.value, item.value);
+                }
+                else {
+                    // @ts-ignore
+                    option.innerHTML = item;
+                    // @ts-ignore
+                    option.value     = item.toString();
+                    mapDomValueToValue.set(option.value, item);
+                }
+                select.appendChild(option);
+                return select;
+        }, document.createElement("select"));
+        if (options.class) select.setAttribute("class", options.class);
+        const onchange = options.onchange;
+        if (onchange) {
+            select.onchange = event => {
+                onchange( mapDomValueToValue.get( select.value) , event );
+            }
+        }
+
+        if (options.initialValue) {
+            for (let i=0; i<select.options.length; i++) {
+                const item = select.options.item(i);
+                if ( !item ) continue;
+                if (mapDomValueToValue.get(item.value)  === options.initialValue ) {
+                    select.selectedIndex = i;
+                    break;
+                }
+            }
+        }
+        if (options.onui) options.onui(select);
+        const div = document.createElement("div");
+        div.setAttribute("style", "display:inline-block");
+        if (options.label) {
+            // div.appendChild(document.createTextNode(options.label));
+            div.title = options.label;
+        }
+        div.appendChild(select);
+        return div;
+    }
+
+
+    static createCheckbox( name: string, options: CheckboxParam ) {
+        const pane = document.createElement("div");
+        pane.style.display = "inline-block";
+        pane.style.padding = "3px";
+        const label = document.createElement("div");
+        label.style.display = "inline-block";
+        label.style.cursor = "pointer";
+        label.style.userSelect = "none";
+        label.innerText = name;
+        const checkbox = document.createElement("input");
+        if (options.initialValue !== undefined) checkbox.checked = options.initialValue;
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.style.cursor = "pointer";
+        if (options.class) checkbox.setAttribute("class", options.class);
+        const onchange = options.onchange;
+        if (onchange) checkbox.onchange = (event: Event) => {
+            onchange( checkbox.checked, event );
+        }
+        label.onclick = event => { checkbox.click(); };
+        pane.append(checkbox);
+        pane.append(label);
+        if (options.onui) options.onui(checkbox);
+        return pane;
+    }
+
+    static createCheckboxOption( option: Option, key: string ) {
+        const property = option.getProperty(key);
+        if (property.type !== "boolean") throw new Error("unsupported type: " + property.type);
+        const checkbox = this.createCheckbox(key, {
+                initialValue: option.get(key),
+                onui: ui => {
+                    option.onChange(key, event => {
+                            if (ui.checked != event.value) ui.checked = event.value;
+                    });
+                },
+                onchange: (value, event) => {
+                    option.set(key, value);
+                }
+        });
+        return checkbox;
+    }
+
+    static createSelectOption( option: Option, key: string ) {
+        const property = option.getProperty(key);
+        // if (property.type !== "select") throw new Error("unsupported type: " + property.type);
+        if ( !Option.isSelectPropertyProp(property) ) throw new Error("unsupported type: " + property.type);
+        return this.createSelect(property.options, {
+                initialValue: property.value,
+                label: key + "\n" + property.description,
+                onui: ui => {
+                    option.onChange(key, event => {
+                            for (let i=0; i<ui.options.length; i++) {
+                                const item = ui.options.item(i);
+                                if (!item) continue;
+                                if (item.value === event.value) {
+                                    if (ui.selectedIndex != i) ui.selectedIndex = i;
+                                    break;
+                                }
+                            }
+                    })
+                },
+                onchange: (value, domEvent) => {
+                    option.set(key, value);
+                }
+        });
+    }
+}
+
+
+
+namespace Option {
+
+
+
+export interface Event<T> {
+    key: string;
+    value: T;
+}
+
+
+export type Handler<T> = (event: Event<T>) => void;
+
+
+export interface PropertyProp<T> {
+    key: string;
+    type: string;
+    value?: T;
+    description?: string;
+    handlers: Handler<T>[];
+}
+
+export interface SelectPropertyProp<T> extends PropertyProp<T> {
+    type: "select";
+    options: (T|LabeledValueObject<T>)[];
+    valueToOptionMap: Map<T, LabeledValueObject<T>>;
+    optionOfValue: (item: T) => LabeledValueObject<T>;
+}
+export function isSelectPropertyProp<T>(obj: PropertyProp<T>): obj is SelectPropertyProp<T> {
+    return obj.type === "select";
+}
+
+export interface Property<T> {
+    key: string;
+    type: string;
+    value?: T;
+    description?: string;
+    handlers?: Handler<T>[];
+}
+export interface SelectProperty<T> extends Property<T> {
+    type: "select";
+    options: (T|LabeledValueObject<T>)[];
+}
+export function isSelectProperty<T>(obj: Property<T>): obj is SelectProperty<T> {
+    return obj.type === "select";
+}
+
+
+export interface LabeledValueObject<T> {
+    label: string;
+    value: T;
+}
+export function isLabeledValueObject(obj: any): obj is LabeledValueObject<any> {
+    return obj.value !== undefined;
+}
+
+
+}
+
+
+export default Option;
+export { DomTool };
