3672d3fdd26870604af2f9100cfc52db46da192c
masa-sony <Masaru.Okaya@sony.com>
2021-07-14 01:08:01 +0900

    typescript: debug: update point-cloud

--- debug/point-cloud/src/index.ts
+++ debug/point-cloud/src/index.ts
@@ -2,23 +2,25 @@ import '../../../packages/ui/dist/mapray.css';
 import App from './app';
 
 // 現在のアプリインスタンス
-var appInstance;
+var appInstance: App | undefined;
 
 
 /**
  * アプリを開始
  * @param  {string|Element} container  コンテナ (ID または要素)
  */
-function startApp( container )
+function startApp( container: HTMLElement | string )
 {
     if ( appInstance ) {
         // すでに動作していれば停止して消去
         appInstance.destroy();
-        appInstance = null;
+        appInstance = undefined;
     }
     appInstance = new App( container );
 }
 
 
+
 // グローバル関数に登録
+// @ts-ignore
 window.startApp = startApp;
