1d92fbea8937796894aa9d3a0a2162a3d1cf4bbb
masa-sony <Masaru.Okaya@sony.com>
2021-07-14 22:33:34 +0900

    typescript: add additional typescript lib

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -2,7 +2,8 @@ import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import typescript from '@rollup/plugin-typescript';
+// import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json'
 
