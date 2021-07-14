700de244ec8fbdf654f6167b72b593d998d4ec39
masa-sony <Masaru.Okaya@sony.com>
2021-07-14 15:34:50 +0900

    typescript: apps: modified uievents

--- debug/uievnets/rollup.config.js
+++ debug/uievnets/rollup.config.js
@@ -1,122 +1,68 @@
-import path from 'path'
-import { terser } from 'rollup-plugin-terser'
-import babel from 'rollup-plugin-babel'
-import { string } from 'rollup-plugin-string'
-import replace from 'rollup-plugin-replace'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+import { string } from 'rollup-plugin-string';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+// import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
-var outdir = "dist/"
 
-let accessToken = '"<your access token here>"'
-if (process.env.MAPRAY_ACCESS_TOKEN)  {
-    accessToken = JSON.stringify(process.env.MAPRAY_ACCESS_TOKEN)
-}
 
-let bingAccessToken = '"<your Bing Maps Key here>"'
-if (process.env.BINGMAP_ACCESS_TOKEN)  {
-    bingAccessToken = JSON.stringify(process.env.BINGMAP_ACCESS_TOKEN)
-}
+const outdir = "dist/";
 
-const localConfig = (appDir) => (
-    [
-        replace({
-            '"@mapray/mapray-js"': JSON.stringify(appDir+"../node_modules/@mapray/mapray-js/src/index.js"),
-            delimiters: ['', ''],
-            exclude: appDir+"../packages/"
-        }),
-        replace({
-            '"@mapray/mapray-js"': JSON.stringify(appDir+"../../node_modules/@mapray/mapray-js/src/index.js"),
-            delimiters: ['', ''],
-            include: appDir+"../packages/"
-        }),
-        replace({
-            '"@mapray/ui"': JSON.stringify(appDir+"../node_modules/@mapray/ui/src/index.js"),
-            delimiters: ['', ''],
-            exclude: appDir+"../packages/"
-        })
-    ]
-)
 
-const getPluginsConfig = (prod) => {
-    const params = [
-        postcss(),
-        replace({
-            '"<your access token here>"': accessToken,
-            delimiters: ['', '']
-        }),
-        replace({
-            '"<your Bing Maps Key here>"': bingAccessToken,
-            delimiters: ['', '']
-        }),
-        commonjs(),
-        resolve(),
-        string({
-            include: [
-                '../../**/*.vert',
-                '../../**/*.frag',
-            ]
-        }),
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
 
-    if (prod) {
-        params.push(
-            terser({
-                compress: {
-                    unused: false,
-                    collapse_vars: false,
-                },
-                output: {
-                    comments: false,
-                },
-            })
-        )
-    }
+export default function() {
 
-    return params;
-}
+    const isProd = process.env.BUILD === 'production';
+    const maprayAccessToken = process.env.MAPRAY_ACCESS_TOKEN;
+    const bingAccessToken = process.env.BINGMAP_ACCESS_TOKEN;
 
-const config = (build) => {
     const bundle = {
-        input: 'src/index.js',
+        input: 'src/index.ts',
         output: {
-            file: outdir+'bundle.js',
+            file: outdir + 'bundle.js',
             format: 'iife',
             indent: false,
-            sourcemap: true
-        }
+            sourcemap: !isProd,
+            name: 'startApp',
+        },
+        plugins: [
+            postcss(),
+            (maprayAccessToken ?
+                replace({
+                        '"<your access token here>"': JSON.stringify( maprayAccessToken ),
+                        delimiters: ['', ''],
+                }):
+                null
+            ),
+            (bingAccessToken ?
+                replace({
+                        '"<your Bing Maps Key here>"': JSON.stringify( bingAccessToken ),
+                        delimiters: ['', ''],
+                }):
+                null
+            ),
+            resolve(),
+            typescript({
+                "tsconfig": './tsconfig.json',
+                "sourceMap": true,
+            }),
+            (isProd ?
+                terser({
+                        compress: {
+                            unused: false,
+                            collapse_vars: false,
+                        },
+                        output: {
+                            comments: false,
+                        },
+                }):
+                null
+            ),
+        ],
     }
-    bundle.plugins = getPluginsConfig(build)
-    return bundle;
-}
-
-// get the setting when developing in local environment
-const loadLocalSetting = (env) => {
-    const appDir = path.join(__dirname, '../')
-    let bundle = config(env.BUILD)
-    bundle = addLocalSettings(env, appDir, bundle)
-    return bundle
-}
-
-const addLocalSettings= (env, appDir, bundle) => {
-    let plugins = bundle.plugins.concat() // copy
-    // should insert first replace method
-    let index = plugins.findIndex(item => JSON.stringify(item) === '{"name":"commonjs"}' ? true : false)
-
-    if (index === -1) {
-        index = 0;
-    }
-
-    Array.prototype.splice.apply(plugins, [index, 0].concat(localConfig(appDir)))
-    bundle.plugins = Object.assign(bundle.plugins, plugins)
 
     return bundle;
-}
-
-module.exports = () => {
-    return loadLocalSetting(process.env)
 }
--- debug/uievnets/src/index.ts
+++ debug/uievnets/src/index.ts
@@ -2,23 +2,23 @@ import '../../../packages/ui/dist/mapray.css';
 import App from './app';
 
 // 現在のアプリインスタンス
-var appInstance;
+let appInstance: App | undefined;
 
 
 /**
  * アプリを開始
  * @param  {string|Element} container  コンテナ (ID または要素)
  */
-function startApp( container )
+function startApp( container: string | HTMLElement )
 {
     if ( appInstance ) {
         // すでに動作していれば停止して消去
         appInstance.destroy();
-        appInstance = null;
+        appInstance = undefined;
     }
     appInstance = new App( container );
 }
 
 
 // グローバル関数に登録
-window.startApp = startApp;
+export default startApp;
