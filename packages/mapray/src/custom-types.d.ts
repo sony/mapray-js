9d8af7724b77a23851dc00f8f7455624be8f7533
masa-sony <Masaru.Okaya@sony.com>
2021-07-28 10:59:04 +0900

    add classes for atmosphere

---
+++ packages/mapray/src/custom-types.d.ts
@@ -0,0 +1,16 @@
+// Load shader codes as string
+
+declare module "*.vert" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.frag" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.glsl" {
+  const content: string;
+  export default content;
+}
