d60157fd91c62ab82c1280a96b9299e750a3745c
masa-sony <Masaru.Okaya@sony.com>
2021-08-19 20:37:50 +0900

    refactor util/Color (use Vector3 or Vector4 instead of Color class. Color class still has calcuration methods)

--- packages/mapray/src/util/Color.ts
+++ packages/mapray/src/util/Color.ts
@@ -1,157 +1,132 @@
-import { Vector3, Vector4 } from "../GeoMath";
+import GeoMath, { Vector3, Vector4 } from "../GeoMath";
 
 
 /**
  * Utility Class for Color
  */
-class Color {
-    /** The red (0.0 ~ 1.0) */
-    private _r: number;
+namespace Color {
 
-    /** The green (0.0 ~ 1.0) */
-    private _g: number;
 
-    /** The blue (0.0 ~ 1.0) */
-    private _b: number;
 
-    /** The alpha (0.0 ~ 1.0) */
-    private _a: number;
+/**
+ * 不透明色を生成。
+ *
+ * 4番目以降の要素は無視される。要素数が 3 に満たない場合は 0.0 と解釈する。
+ */
+export function createOpaqueColor( rgb?: Vector3 ): Vector3 {
+    return GeoMath.createVector3( rgb );
+}
 
-    /**
-     * @param r The red (0.0 ~ 1.0)
-     * @param g The green (0.0 ~ 1.0)
-     * @param b The blue (0.0 ~ 1.0)
-     * @param a The alpha (0.0 ~ 1.0)
-     */
-    constructor( r: number, g: number, b: number, a: number ) 
-    {
-        this._r = r;
-        this._g = g;
-        this._b = b;
-        this._a = a;
+
+/**
+ * 色を生成
+ */
+export function createColor( rgb?: Vector3 | Vector4 ): Vector4 {
+    return GeoMath.createVector4(
+        !rgb ? undefined :
+        GeoMath.isVector4( rgb ) ? rgb :
+        [ rgb[0], rgb[1], rgb[2], 1.0 ]
+    );
+}
+
+
+/**
+ * 不透明色を代入
+ *
+ * src を dst に代入する。
+ * 透明色を代入した場合は、透明チャネルが無視される。
+ * @param  src  代入元
+ * @param  dst  代入先
+ * @return dst
+ */
+export function copyOpaqueColor( src: Vector3 | Vector4, dst: Vector3 ): Vector3
+{
+    dst[0] = src[0];
+    dst[1] = src[1];
+    dst[2] = src[2];
+    return dst;
+}
+
+
+/**
+ * 色を代入
+ *
+ * src を dst に代入する。
+ * 不透明色を代入した場合は、透明チャネルは `1.0` とする。
+ * @param  src  代入元
+ * @param  dst  代入先
+ * @return dst
+ */
+export function copyColor( src: Vector3 | Vector4, dst: Vector4 ): Vector4
+{
+    dst[0] = src[0];
+    dst[1] = src[1];
+    dst[2] = src[2];
+    dst[3] = GeoMath.isVector3( src ) ? 1.0 : src[3];
+    return dst;
+}
+
+
+/**
+ * 乗算済みアルファを計算します。
+ *
+ * @param  src  入力値
+ * @param  dst  代入先
+ * @return dst
+ */
+export function premultiply( src: Vector3 | Vector4, dst: Vector4 ): Vector4
+{
+    if ( GeoMath.isVector4( src ) ) {
+        const alpha = src[3];
+        if ( alpha === 0 ) {
+            dst[0] = dst[1] = dst[2] = dst[3] = 0.0;
+        }
+        else {
+            dst[0] = src[0] / alpha;
+            dst[1] = src[1] / alpha;
+            dst[2] = src[2] / alpha;
+            dst[3] = alpha;
+        }
     }
-
-    /**
-     * 不透明色を生成
-     */
-    static generateOpacityColor( rgb: Vector3 ): Color {
-        return new Color(rgb[0], rgb[1], rgb[2], 1)
+    else {
+        dst[0] = src[0];
+        dst[1] = src[1];
+        dst[2] = src[2];
+        dst[3] = 1.0;
     }
+    return dst;
+}
 
-    /**
-     * 色を代入
-     *
-     * src を dst に代入する。
-     * @param  src  代入元
-     * @param  dst  代入先
-     * @return dst
-     */
-    static copyColor( src: Color, dst: Color ): Color
-    {
-        dst._r = src._r;
-        dst._g = src._g;
-        dst._b = src._b;
-        dst._a = src._a;
 
-        return dst;
+/**
+ * RGBA文字列に変換する
+ */
+export function toRGBString( rgb: Vector3 | Vector4 ): string
+{
+    const r = Color.floatToByte( rgb[0] );
+    const g = Color.floatToByte( rgb[1] );
+    const b = Color.floatToByte( rgb[2] );
+    if ( GeoMath.isVector4( rgb ) ) {
+        const a = Color.floatToByte( rgb[3] );
+        return `rgba(${r},${g},${b},${a})`;
     }
-
-    /**
-     * 不透明色を代入
-     *
-     * src を dst に代入する。
-     * @param  rgb  代入元
-     * @param  dst  代入先
-     */
-    static setOpacityColor( rgb: Vector3, dst: Color )
-    {
-        dst._r = rgb[0];
-        dst._g = rgb[1];
-        dst._b = rgb[2];
-        dst._a = 1;
+    else {
+        return `rgb(${r},${g},${b})`;
     }
+}
 
-    /**
-     * 色配列に変換する
-     *
-     * 0から255に正規化。 [R, G, B, A]の順番
-     */
-    toArray(): Vector4
-    {
-        return (this._a === 0 ? [0, 0, 0, 0] : [
-            this.floatToByte(this._r) / this._a, 
-            this.floatToByte(this._g) / this._a, 
-            this.floatToByte(this._b) / this._a, 
-            this._a
-        ]); 
-    }
+/**
+ * 0~1.0 の色値を 255 までの値に正規化
+ */
+export function floatToByte( value: number ): number
+{
+    return value === 1.0 ? 255.0 : (value * 256.0) | 0;
+}
 
-    /**
-     * 色配列に変換する
-     *
-     * 0から1に正規化。 [R, G, B, A]の順番
-     *
-     * @return dst
-     */
-    toVector4(): Vector4
-    {
-        return (this._a === 0 ? [0, 0, 0, 0] : [
-            this._r / this._a, 
-            this._g / this._a, 
-            this._b / this._a, 
-            this._a
-        ]);
-    }
 
-    /**
-     * RGBA文字列に変換する
-     *
-     * RGBA文字列に変換して文字列を返却
-     */
-    toRGBString(): string
-    {
-        const rgba = this.toArray();
-        return `rgba(${Math.round(rgba[0])},${Math.round(rgba[1])},${Math.round(rgba[2])},${rgba[3]})`;
-    }
 
-    /**
-     * 0~1.0の色値を255までで正規化
-     *
-     * 0から1で正規化された色値を255までに拡張する
-     */
-    floatToByte( value: number ): number
-    {
-        return value === 1.0 ? 255.0 : (value * 256.0) | 0;
-    }
+} // namespace Color
 
-    /**
-     * Red
-     */
-    get r(): number {
-        return this._r;
-    }
 
-    /**
-     * Green
-     */
-    get g(): number {
-        return this._g;
-    }
-
-     /**
-     * Blue
-     * number
-     */
-    get b(): number {
-        return this._b;
-    }
-
-    /**
-     * Alpha
-     */
-    get a(): number {
-        return this._a;
-    }
-};
 
 export default Color;
