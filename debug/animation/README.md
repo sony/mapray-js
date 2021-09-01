030d4918579b96deb9b5008d1baf14099b889992
onishi <onishi@sofcube.co.jp>
2021-09-01 18:18:41 +0900

    add animation app (debug/animation)

---
+++ debug/animation/README.md
@@ -0,0 +1,16 @@
+Animation Example
+================================================================================
+
+
+Setup
+--------------------------------------------------------------------------------
+### Setup tokens
+
+1. mapray access token(`MAPRAY_ACCESS_TOKEN`) and bing map access token(`BINGMAP_ACCESS_TOKEN`) needs to be defined.
+2. `yarn` to install dependencies
+
+
+Launch
+--------------------------------------------------------------------------------
+- `yarn start` to start watch and server
+- `yarn build-prod` to build in production mode
---
+++ debug/animation/rollup.config.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+// import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+
+
+
+const outdir = "dist/";
+
+
+
+export default function() {
+
+    const isProd = process.env.BUILD === 'production';
+    const maprayAccessToken = process.env.MAPRAY_ACCESS_TOKEN;
+    const bingAccessToken = process.env.BINGMAP_ACCESS_TOKEN;
+
+    const bundle = {
+        input: 'src/App.ts',
+        output: {
+            file: outdir + 'bundle.js',
+            format: 'iife',
+            indent: false,
+            sourcemap: !isProd,
+            name: "App",
+        },
+        plugins: [
+            postcss(),
+            (maprayAccessToken ?
+                replace({
+                    '"<your access token here>"': JSON.stringify( maprayAccessToken ),
+                    delimiters: ['', ''],
+                }):
+                null
+            ),
+            (bingAccessToken ?
+                replace({
+                    '"<your Bing Maps Key here>"': JSON.stringify( bingAccessToken ),
+                    delimiters: ['', ''],
+                }):
+                null
+            ),
+            resolve(),
+            typescript({
+                tsconfig: './tsconfig.json',
+                tsconfigOverride: {
+                    compilerOptions: {
+                        sourceMap: true,
+                    }
+                }
+            }),
+            (isProd ?
+                terser({
+                    compress: {
+                        unused: false,
+                        collapse_vars: false,
+                    },
+                    output: {
+                        comments: false,
+                    },
+                }):
+                null
+            ),
+        ],
+    }
+
+    return bundle;
+}
