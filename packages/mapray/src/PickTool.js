6e70922f47e3664580ef8ad890bff6cc6b7c5a7a
masa-sony <Masaru.Okaya@sony.com>
2022-01-24 12:18:01 +0900

    support picking point cloud (supported maximum radius is 10px)

--- packages/mapray/src/PickTool.js
+++ packages/mapray/src/PickTool.js
@@ -18,13 +18,16 @@ class PickTool {
     /**
      * @param {mapray.GLEnv} glenv     WebGL 環境
      */
-    constructor( glenv ) {
+    constructor( glenv, option={} ) {
         this._glenv = glenv;
         const gl = this._glenv.context;
 
-        this._camera = new Camera({ width: 1, height: 1 });
+        this._width  = option.width  || 11;
+        this._height = option.height || 11;
 
-        this._frame_buffer = new FrameBuffer( this._glenv, 1, 1, {
+        this._camera = new Camera({ width: this._width, height: this._height });
+
+        this._frame_buffer = new FrameBuffer( this._glenv, this._width, this._height, {
                 color_containers: [{
                         type: FrameBuffer.ContainerType.RENDER_BUFFER,
                         options: {
@@ -42,7 +45,7 @@ class PickTool {
                 },
         });
 
-        this._depth_to_color_frame_buffer = new FrameBuffer( this._glenv, 1, 1, {
+        this._depth_to_color_frame_buffer = new FrameBuffer( this._glenv, this._width, this._height, {
                 color_containers: [{
                         type: FrameBuffer.ContainerType.RENDER_BUFFER,
                         options: {
@@ -102,8 +105,8 @@ class PickTool {
             };
         }
 
-        this._rid_value   = new Uint8Array( 4 );
-        this._depth_value = new Uint8Array( 4 );
+        this._rid_value   = new Uint8Array( 4 * this._width * this._height );
+        this._depth_value = new Uint8Array( 4 * this._width * this._height );
     }
 
     /**
@@ -115,7 +118,9 @@ class PickTool {
         const ch = viewer_camera.canvas_size.height;
         this._camera.copyViewParameters( viewer_camera );
         const hfov_rad = viewer_camera.fov * GeoMath.DEGREE / 2;
-        const hfov_rad2 = Math.atan( Math.sqrt( 2 ) * Math.tan( hfov_rad ) / Math.sqrt( cw*cw + ch*ch ) );
+        const cw2 = this._camera.canvas_size.width;
+        const ch2 = this._camera.canvas_size.height;
+        const hfov_rad2 = Math.atan( Math.sqrt( cw2*cw2 + ch2*ch2 ) * Math.tan( hfov_rad ) / Math.sqrt( cw*cw + ch*ch ) );
         this._camera.fov  = 2 * hfov_rad2 / GeoMath.DEGREE;
         return this._camera;
     }
@@ -154,7 +159,7 @@ class PickTool {
 
         startRidRead = Date.now();
 
-        gl.readPixels( 0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, this._rid_value );
+        gl.readPixels( 0, 0, this._width, this._height, gl.RGBA, gl.UNSIGNED_BYTE, this._rid_value );
         endRidRead = Date.now();
         // 4bit x4 の値が 8bit x4 に格納されている。
         /*
@@ -165,11 +170,12 @@ class PickTool {
             this._rid_value[3] / 17
         );
         */
+        const offset = 4 * Math.floor( this._width * this._height / 2 );
         const rid = Math.round(
-            COEFFICIENTS_RID[ 0 ] * Math.round(this._rid_value[ 0 ] / 17.0) +
-            COEFFICIENTS_RID[ 1 ] * Math.round(this._rid_value[ 1 ] / 17.0) +
-            COEFFICIENTS_RID[ 2 ] * Math.round(this._rid_value[ 2 ] / 17.0) +
-            COEFFICIENTS_RID[ 3 ] * Math.round(this._rid_value[ 3 ] / 17.0)
+            COEFFICIENTS_RID[ 0 ] * Math.round(this._rid_value[ offset + 0 ] / 17.0) +
+            COEFFICIENTS_RID[ 1 ] * Math.round(this._rid_value[ offset + 1 ] / 17.0) +
+            COEFFICIENTS_RID[ 2 ] * Math.round(this._rid_value[ offset + 2 ] / 17.0) +
+            COEFFICIENTS_RID[ 3 ] * Math.round(this._rid_value[ offset + 3 ] / 17.0)
         );
 
         this._frame_buffer.unbind();
@@ -199,7 +205,7 @@ class PickTool {
         this._depth_to_color_frame_buffer.bind();
 
         let depth_clip = 0;
-        gl.viewport( 0, 0, 1, 1 );
+        gl.viewport( 0, 0, this._width, this._height );
         for ( let i=0; i<2; i++ ) {
             const material = this._depth_to_color_materials[ i ];
             material.bindProgram();
@@ -212,7 +218,7 @@ class PickTool {
             gl.bindTexture(gl.TEXTURE_2D, null);
 
             startDepthRead = Date.now();
-            gl.readPixels( 0, 0, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, this._depth_value );
+            gl.readPixels( 0, 0, this._width, this._height, gl.RGBA, gl.UNSIGNED_BYTE, this._depth_value );
             endDepthRead = Date.now();
 
             // 4bit x4 の値が 8bit x4 に格納されている。 => [0.0 〜 1.0]
