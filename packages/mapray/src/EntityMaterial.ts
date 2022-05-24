ab15ef2c10f407cb1aa2e1830cae225e9876eb00
Yasuhisa Tanioka <Yasuhisa.Tanioka@sony.com>
2022-05-24 18:43:27 +0900

    typescript: Primitive, EntityMaterial

---
+++ packages/mapray/src/EntityMaterial.ts
@@ -0,0 +1,131 @@
+import Material from "./Material";
+import GeoMath from "./GeoMath";
+import RenderStage from "./RenderStage";
+import GLEnv from "./GLEnv";
+import type Primitive from "./Primitive";
+import { cfa_assert } from "./util/assertion";
+
+
+/**
+ * エンティティ・マテリアル
+ *
+ * このクラスは、mapray.RenderStage.getRenderTarget()の値により異なる動作をする。
+ * <ul>
+ * <li>
+ * mapray.RenderStage.RenderTarget.SCENEの場合は、通常通り描画を行う。
+ * setParametersは、描画に必要な全てのパラメータを設定します。
+ * <li>mapray.RenderStage.RenderTarget.RIDの場合は、
+ * setParametersは、RID描画に必要なパラメータのみ設定します（一般にテクスチャや色情報は除外される）。
+ * このクラスでの実装は、setParameters()により、u_ridが設定されるようになっています。
+ * </ul>
+ */
+abstract class EntityMaterial extends Material {
+
+    /**
+     * @param glenv   - WebGL 環境
+     * @param vs_code - 頂点シェーダのソースコード
+     * @param fs_code - フラグメントシェーダのソースコード
+     */
+    protected constructor( glenv:   GLEnv,
+                           vs_code: string,
+                           fs_code: string )
+    {
+        super( glenv, vs_code, fs_code );
+    }
+
+
+    /**
+     * 背景との混合が必要か？
+     *
+     * @param stage     - レンダリングステージ
+     * @param primitive - プリミティブ
+     *
+     * @return 背景との混合が必要なとき true, それ以外のとき false
+     *
+     * @default false
+     *
+     * @virtual
+     */
+    isTranslucent( stage:   RenderStage,
+                   primitive: Primitive ): boolean
+    {
+        return false;
+    }
+
+
+    /**
+     * マテリアルパラメータを設定
+     *
+     * 事前に `material.bindProgram()` すること。
+     *
+     * @param stage     - レンダリングステージ
+     * @param primitive - プリミティブ
+     *
+     * @virtual
+     */
+    setParameters( stage:   RenderStage,
+                   primitive: Primitive ): void
+    {
+        if (stage.getRenderTarget() === RenderStage.RenderTarget.RID) {
+            cfa_assert( primitive.rid !== undefined );
+            this._setRenderId( primitive.rid );
+        }
+    }
+
+
+    /**
+     * `u_obj_to_clip` 変数を設定
+     *
+     * @param stage     - レンダリングステージ
+     * @param primitive - プリミティブ
+     */
+    protected setObjToClip( stage:   RenderStage,
+                            primitive: Primitive ): void
+    {
+        var obj_to_gocs = primitive.transform;
+        var obj_to_clip = EntityMaterial._obj_to_clip;
+
+        // obj_to_clip = gocs_to_clip * obj_to_gocs
+        GeoMath.mul_GA( stage.gocs_to_clip, obj_to_gocs, obj_to_clip );
+
+        this.setMatrix( "u_obj_to_clip", obj_to_clip );
+    }
+
+
+    /**
+     * `u_obj_to_view` 変数を設定
+     *
+     * @param stage     - レンダリングステージ
+     * @param primitive - プリミティブ
+     */
+    protected setObjToView( stage:   RenderStage,
+                            primitive: Primitive ): void
+    {
+        var obj_to_gocs = primitive.transform;
+        var obj_to_view = EntityMaterial._obj_to_view;
+
+        // obj_to_view = gocs_to_view * obj_to_gocs
+        GeoMath.mul_AA( stage.gocs_to_view, obj_to_gocs, obj_to_view );
+
+        this.setMatrix( "u_obj_to_view", obj_to_view );
+    }
+
+    /**
+     */
+    private _setRenderId( id: number ) {
+        this.setVector4( "u_rid", [
+                (id >> 12 & 0xF) / 0xF,
+                (id >>  8 & 0xF) / 0xF,
+                (id >>  4 & 0xF) / 0xF,
+                (id       & 0xF) / 0xF
+        ]);
+    }
+
+
+    private static readonly _obj_to_clip = GeoMath.createMatrixf();  // 計算用一時領域
+    private static readonly _obj_to_view = GeoMath.createMatrixf();  // 計算用一時領域
+
+}
+
+
+export default EntityMaterial;
---
+++ packages/mapray/src/Primitive.ts
@@ -0,0 +1,291 @@
+import GeoMath, { Vector3, Vector4, Matrix } from "./GeoMath";
+import GLEnv from "./GLEnv";
+import Mesh from "./Mesh";
+import type EntityMaterial from "./EntityMaterial";
+import type RenderStage from "./RenderStage";
+
+
+/**
+ * 描画プリミティブ
+ *
+ * @see mapray.PropSet
+ * @see mapray.Entity#getPrimitives
+ */
+class Primitive {
+
+    /**
+     * 描画されるメッシュ
+     *
+     * 構築子の `mesh` 引数が設定される。
+     */
+    mesh: Mesh;
+
+
+    /**
+     * 描画に使用するマテリアル
+     *
+     * 構築子の `material` 引数が設定される。
+     */
+    material: EntityMaterial;
+
+
+    /**
+     * モデル座標系から GOCS への変換行列
+     *
+     * 構築子の `transform` 引数が設定される。
+     */
+    transform: Matrix;
+
+
+    /**
+     * 中心点 (モデル座標系)
+     *
+     * `null` のときは零ベクトルと見なす。
+     *
+     * @default null
+     */
+    pivot: Vector3 | null;
+
+
+    /**
+     * 境界箱 (モデル座標系)
+     *
+     * `bbox[0]` は座標の最小値、`bbox[1]` は座標の最大値とする。
+     *
+     * `null` のときは無限大の境界箱と見なす。
+     *
+     * @default null
+     */
+    bbox: Vector3[] | null;
+
+
+    /**
+     * プロパティ集合
+     *
+     * `null` のときは空集合と見なす。
+     *
+     * @default null
+     */
+    properties: object | null;
+
+
+    /**
+     * `RenderTarget.RID` のときだけ存在する
+     */
+    rid?: number;
+
+
+    /**
+     * ソート深度
+     *
+     * @internal
+     */
+    sort_z: number;
+
+
+    /**
+     * 引数に `null` を指定した場合は、`this` を使う前に対応するプロパ
+     * ティに適切なオブジェクトを設定しなければならない。
+     *
+     * @param glenv     - WebGL 環境
+     * @param mesh      - メッシュ
+     * @param material  - マテリアル
+     * @param transform - 変換行列
+     */
+    constructor( glenv:     GLEnv,
+                 mesh:      Mesh | null,
+                 material:  EntityMaterial | null,
+                 transform: Matrix | null )
+    {
+        this._glenv = glenv;
+
+        // 後で適切なオブジェクトが設定されることが約束されている
+        // @ts-ignore
+        this.mesh = mesh;
+
+        // 後で適切なオブジェクトが設定されることが約束されている
+        // @ts-ignore
+        this.material = material;
+
+        // 後で適切なオブジェクトが設定されることが約束されている
+        // @ts-ignore
+        this.transform = transform;
+
+        this.pivot      = null;
+        this.bbox       = null;
+        this.properties = null;
+        this.sort_z     = 0;
+    }
+
+
+    /**
+     * インスタンスの複製を返す
+     *
+     * 公開プロパティが `this` と同じインスタンスを生成して返す。
+     *
+     * ただしプロパティ `mesh`, `material`, `properties` は参照コピー
+     * で、それ以外は深いコピーとなる。
+     *
+     * @return インスタンスの複製
+     */
+    fastClone(): Primitive
+    {
+        const clone = new Primitive( this._glenv, this.mesh, this.material, GeoMath.createMatrix( this.transform ) );
+
+        if ( this.pivot ) {
+            clone.pivot = GeoMath.createVector3( this.pivot );
+        }
+
+        if ( this.bbox ) {
+            clone.bbox = this.bbox.map( v => GeoMath.createVector3( v ) );
+        }
+
+        clone.properties = this.properties;
+
+        return clone;
+    }
+
+
+    /**
+     * プリミティブが見えるか？
+     *
+     * `true` を返したときはソート深度 `this.sort_z` が設定される。
+     *
+     * @param stage - レンダリングステージ
+     *
+     * @return プリミティブの一部が視体積に含まれるとき true, それ以外
+     *         のとき false
+     */
+    isVisible( stage: RenderStage ): boolean
+    {
+        // obj_to_view = stage._gocs_to_view * this.transform
+        const matrix = temp_obj_to_view;
+        GeoMath.mul_AA( stage.gocs_to_view, this.transform, matrix );
+
+        const bbox = this.bbox;
+        if ( bbox ) {
+            // 境界箱の頂点座標を変換 (視点空間) -> bbox_points
+            transformBBox( bbox, matrix );
+
+            // 視体積平面と比較
+            for ( const plane of stage.getVolumePlanes() ) {
+                if ( isBBoxBackSide( plane ) ) {
+                    // 完全に視体積に含まれない
+                    return false;
+                }
+            }
+        }
+
+        // ソート深度を設定
+        const pivot = this.pivot;
+        if ( pivot ) {
+            this.sort_z = matrix[2]*pivot[0] + matrix[6]*pivot[1] + matrix[10]*pivot[2] + matrix[14];
+        }
+        else {
+            this.sort_z = matrix[14];
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 背景との混合が必要か？
+     *
+     * @param stage - レンダリングステージ
+     *
+     * @return 背景との混合が必要なとき `true`, それ以外のとき `false`
+     *
+     */
+    isTranslucent( stage: RenderStage ): boolean
+    {
+        return this.material.isTranslucent( stage, this );
+    }
+
+
+    /**
+     * プリミティブを描画
+     *
+     * @param stage - レンダリングステージ
+     */
+    draw( stage: RenderStage ): void
+    {
+        const material = this.material;
+        material.bindProgram();
+        material.setParameters( stage, this );
+        this.mesh.draw( material );
+    }
+
+
+    private readonly _glenv: GLEnv;
+
+}
+
+
+/**
+ * 境界箱の頂点座標を変換 (視点空間) -> bbox_points
+ */
+function transformBBox( bbox:   Vector3[],
+                        matrix: Matrix ): void
+{
+    for ( let iz = 0; iz < 2; ++iz ) {
+        const zm = bbox[iz][2];
+        for ( let iy = 0; iy < 2; ++iy ) {
+            const ym = bbox[iy][1];
+            for ( let ix = 0; ix < 2; ++ix ) {
+                const xm = bbox[ix][0];
+                const bbox_point = temp_bbox_points[ix + 2*iy + 4*iz];
+                bbox_point[0] = matrix[0]*xm + matrix[4]*ym + matrix[ 8]*zm + matrix[12];
+                bbox_point[1] = matrix[1]*xm + matrix[5]*ym + matrix[ 9]*zm + matrix[13];
+                bbox_point[2] = matrix[2]*xm + matrix[6]*ym + matrix[10]*zm + matrix[14];
+            }
+        }
+    }
+}
+
+
+/**
+ * bbox_points はすべて plane の裏側か？
+ */
+function isBBoxBackSide( plane: Vector4 ): boolean
+{
+    for ( const point of temp_bbox_points ) {
+        const dist = point[0]*plane[0] + point[1]*plane[1] + point[2]*plane[2] + plane[3];
+        if ( dist >= 0 ) {
+            // 表側に頂点が存在
+            return false;
+        }
+    }
+
+    return true;  // すべての頂点が裏側
+}
+
+
+// 一時領域
+// モデル空間から視点空間への変換行列
+const temp_obj_to_view = GeoMath.createMatrix();
+
+
+// 一時領域
+// 境界箱の頂点座標 (視点空間)
+const temp_bbox_points: Vector3[] = [];
+
+for ( let i = 0; i < 8; ++i ) {
+    temp_bbox_points.push( GeoMath.createVector3() );
+}
+
+
+/**
+ * @summary プロパティ集合
+ * @desc
+ * <p>プリミティブのプロパティ集合を表現する。</p>
+ * <p>props を PropSet のインスタンス、name をプロパティ名とするとき、props.name または props["name"]
+ * でプロパティ名からプロパティ値を取得することができる。</p>
+ * <p>このクラスは実際には存在せず、一般的に Object で代用することができる。</p>
+ * @class mapray.PropSet
+ * @private
+ * @see mapray.Primitive
+ */
+
+
+export default Primitive;
